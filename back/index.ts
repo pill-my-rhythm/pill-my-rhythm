2921038f45101f6b6858f779108e0b99f2bf2f3d
parkjungyoon <tbr06057@naver.com>
2022-06-03 16:24:45 +0900

    DB 연결

--- back/index.ts
+++ back/index.ts
@@ -1,12 +1,23 @@
 import app from './src/app';
+import { sequelize } from './src/db/index';
+
 import { createServer } from 'http';
 
 const port: number = Number(process.env.PORT) || 5001;
 
 const server = createServer(app);
 
-server.listen(port, () => {
+server.listen(port, async () => {
   console.log(`${port}포트 서버 대기 중!`);
+
+  await sequelize
+    .authenticate()
+    .then(async () => {
+      console.log('connection success');
+    })
+    .catch((e: Error) => {
+      console.log('TT : ', e);
+    });
 });
 
 export default server;
