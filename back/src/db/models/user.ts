e5ba47f05226f7b74aa814ff8477ffbcaab44d84
SongheeNoh <s0n9h2@gmail.com>
2022-06-08 00:11:41 +0900

    [BE] feat: tb_user 테이블 생성 성공 코드

---
+++ back/src/db/models/user.ts
@@ -0,0 +1,59 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "./index";
+
+// These are all the attributes in the User model
+interface UsersAttributes {
+  pk_user_id: bigint;
+  user_name: string;
+  email: string;
+  password: string | null;
+  gender: string;
+  age_range: number;
+  job: boolean;
+}
+
+export class Users extends Model<UsersAttributes> {}
+
+Users.init(
+  {
+    pk_user_id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    user_name: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING(100),
+      allowNull: false, // 카카오 로그인은 비번 필요없으니 빼기도 하나 봄
+    },
+    gender: {
+      type: DataTypes.ENUM("F", "M"),
+      allowNull: true,
+    },
+    age_range: {
+      type: DataTypes.STRING(10),
+      allowNull: true,
+    },
+    job: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Users",
+    tableName: "tb_user",
+    freezeTableName: true,
+    timestamps: true,
+    paranoid: true, // 삭제일 (복구용)
+    underscored: true,
+  },
+);
