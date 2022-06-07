b8c81697584107a61c8c6dbd71f17255628c2be3
SongheeNoh <s0n9h2@gmail.com>
2022-06-08 00:11:30 +0900

    [BE] add: 세션 관련 코드 삭제...

--- back/src/app.ts
+++ back/src/app.ts
@@ -2,9 +2,6 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 import path from "path";
-import bodyParser from "body-parser";
-import session from "express-session";
-import bcrypt from "bcrypt-nodejs";
 import { UserRouter } from "./routers/userRouter";
 
 const app: express.Application = express();
@@ -13,20 +10,10 @@ const swaggerSpec = YAML.load(path.join(__dirname, "./swagger.yaml"));
 // swagger
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// session
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(
-  session({
-    secret: "shthdgml&qkrwjddbs==BACKEND",
-    resave: false,
-    saveUninitialized: true,
-  }),
-);
-
 app.get("/", (req: express.Request, res: express.Response, next: express.NextFunction) => {
   res.send("hello typescript express!");
 });
 
-app.get("/login", UserRouter);
+app.get("/user", UserRouter);
 
 export default app;
