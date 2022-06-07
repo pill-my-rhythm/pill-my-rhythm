4be59eaa660094e1732374e6485cfb7d167efea9
parkjungyoon <tbr06057@naver.com>
2022-06-07 17:52:54 +0900

    [BE] create: CORS 코드 추가

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 import path from "path";
@@ -6,12 +7,14 @@ import path from "path";
 const app: express.Application = express();
 const swaggerSpec = YAML.load(path.join(__dirname, "./swagger.yaml"));
 
-app.get(
-  "/",
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.send("hello typescript express!");
-  },
-);
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("hello typescript express!");
+});
 
 // swagger
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
