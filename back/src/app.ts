b2124f4d522023c32cd31345a6438899bbaa126c
SongheeNoh <s0n9h2@gmail.com>
2022-06-07 15:12:05 +0900

    [BE] add: 세션 미들웨어 추가

--- back/src/app.ts
+++ back/src/app.ts
@@ -2,18 +2,28 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 import path from "path";
+import bodyParser from "body-parser";
+import session from "express-session";
+import bcrypt from "bcrypt-nodejs";
 
 const app: express.Application = express();
 const swaggerSpec = YAML.load(path.join(__dirname, "./swagger.yaml"));
 
-app.get(
-  "/",
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.send("hello typescript express!");
-  },
-);
-
 // swagger
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// session
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "shthdgml&qkrwjddbs==BACKEND",
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
+
+app.get("/", (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.send("hello typescript express!");
+});
 
 export default app;
