8e4c04c7318798a2bcf003f248389e6471c15e1b
parkjungyoon <tbr06057@naver.com>
2022-06-08 20:08:51 +0900

    [BE] refact: User.ts

--- back/src/db/User.ts
+++ back/src/db/User.ts
@@ -1,18 +1,18 @@
 import { Users } from "./models/user";
 import { IUserInput } from "../interfaces/userInput";
 
-export interface IUserModelInput {
-  pk_user_id: string;
-  user_name: string;
-  email: string;
-  password: string;
-  gender?: string;
-  age_range?: string;
-  job?: string;
-}
+// export interface IUserModelInput {
+//   pk_user_id: string;
+//   user_name: string;
+//   email: string;
+//   password: string;
+//   gender?: string;
+//   age_range?: string;
+//   job?: string;
+// }
 
 class User {
-  static async addUser(newUserData: IUserModelInput) {
+  static async addUser(newUserData: IUserInput) {
     try {
       const user = await Users.create(newUserData);
       return user;
