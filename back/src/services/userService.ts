5d8d25acf16b8ddcef3a8f9f522a70fa75511e0b
parkjungyoon <tbr06057@naver.com>
2022-06-08 22:35:18 +0900

    [BE] refact: let -> const

--- back/src/services/userService.ts
+++ back/src/services/userService.ts
@@ -9,7 +9,7 @@ class userService {
       throw new Error("중복된 이메일입니다. 다른 이메일을 입력해주세요.");
     }
     const hashedPassword = await bcrypt.hash(data.password, 10);
-    let newUserData = { ...data };
+    const newUserData = { ...data };
     newUserData["password"] = hashedPassword;
 
     const newUser = await User.addUser(newUserData);
