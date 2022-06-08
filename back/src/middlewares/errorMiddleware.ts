f91cfd3c5e1fa142401af6f2e1987b2688e2aff0
parkjungyoon <tbr06057@naver.com>
2022-06-08 15:05:24 +0900

    [BE] refact: err -> error로 변경

--- back/src/middlewares/errorMiddleware.ts
+++ back/src/middlewares/errorMiddleware.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 
-function errorMiddleware(err: Error, req: Request, res: Response, next: NextFunction) {
+function errorMiddleware(error: Error, req: Request, res: Response, next: NextFunction) {
   // 터미널에 노란색으로 출력됨.
-  console.log("\x1b[33m%s\x1b[0m", err);
-  res.status(500).json({ message: err.message });
+  console.log("\x1b[33m%s\x1b[0m", error);
+  res.status(500).json({ message: error.message });
 }
 
 export { errorMiddleware };
