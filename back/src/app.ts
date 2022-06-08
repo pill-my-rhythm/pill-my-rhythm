d148050bca6f38edd87031ad7f93691bce6873b4
parkjungyoon <tbr06057@naver.com>
2022-06-08 11:37:39 +0900

    [BE] chore : userRouter 연결

--- back/src/app.ts
+++ back/src/app.ts
@@ -4,6 +4,8 @@ import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 import path from "path";
 
+import { userRouter } from "./routers/userRouter";
+
 const app: express.Application = express();
 const swaggerSpec = YAML.load(path.join(__dirname, "./swagger.yaml"));
 
@@ -15,6 +17,8 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("hello typescript express!");
 });
+
+app.use(userRouter);
 
 // swagger
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
