fdf01ea22b78e9241b131d945ac056c2c6ecf896
YounJoo Seog <reina.seog@gmail.com>
2022-06-08 14:08:18 +0900

    [FE] style : my theme 추가

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#38b7c9",
+          secondary: "#41d1a6",
+          accent: "#ea6493",
+          neutral: "#3D4451",
+          "base-100": "#FFFFFF",
+          info: "#3ABFF8",
+          success: "#6490ea",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 };
