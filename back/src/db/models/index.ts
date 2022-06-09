2a14637eb390103a0743bafcde2f150495377a85
SongheeNoh <s0n9h2@gmail.com>
2022-06-09 23:38:47 +0900

    [BE] fix: sequelize 타임존 한국으로 바꿈

--- back/src/db/models/index.ts
+++ back/src/db/models/index.ts
@@ -4,6 +4,11 @@ import config from "../config/config";
 const sequelize = new Sequelize(config.development.database, config.development.username, config.development.password, {
   host: config.development.host,
   dialect: "mysql",
+  timezone: "+09:00",
+  dialectOptions: { charset: "utf8mb4", dateStrings: true, typeCast: true },
+  define: {
+    timestamps: true,
+  },
 });
 
 export default sequelize;
