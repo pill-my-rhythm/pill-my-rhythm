3c50b6572b718e74d1f58e29cccd29479bf310d2
YounJoo Seog <reina.seog@gmail.com>
2022-06-09 15:19:09 +0900

    [FE] style: header top fix, list로 code 수정, icon 추가

--- front/src/routes/Header/Header.tsx
+++ front/src/routes/Header/Header.tsx
@@ -1,40 +1,40 @@
 const Header = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar w-full bg-base-100 sticky top-0">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">Pill my rhythm</a>
+        <a className="btn btn-ghost normal-case text-xl">
+          <img src="https://blog.kakaocdn.net/dn/bro2IW/btrEji2iHDE/gJHWwqC1zfOCxRpv2cOwP0/img.png" width={30} height={30} className="mr-2" />
+          Pill my rhythm
+        </a>
       </div>
       <div className="flex-none">
-        <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-ghost">
-            <div className="indicator">Search</div>
-          </label>
-          <label tabIndex={0} className="btn btn-ghost">
-            <div className="indicator">Schedular</div>
-          </label>
-          <label tabIndex={0} className="btn btn-ghost">
-            <div className="indicator">Contact us</div>
-          </label>
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost">
-              <div className="w-10">Login</div>
-            </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <ul className="menu menu-horizontal p-0">
+          <li>
+            <a>Search</a>
+          </li>
+          <li>
+            <a>Schedular</a>
+          </li>
+          <li>
+            <a>Contact us</a>
+          </li>
+          <li tabIndex={0}>
+            <a>
+              Login
+              <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+              </svg>
+            </a>
+            <ul className="p-2 bg-base-100">
               <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
+                <a>My Page</a>
               </li>
               <li>
                 <a>Logout</a>
               </li>
             </ul>
-          </div>
-        </div>
+          </li>
+        </ul>
       </div>
     </div>
   );
