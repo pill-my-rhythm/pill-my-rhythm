239560473d6c8a5ad48cb8b1de9c36f9169b3d49
SongheeNoh <s0n9h2@gmail.com>
2022-06-09 16:46:13 +0900

    [BE] add: IUserInfoUpdateInput 인터페이스 추가

--- back/src/interfaces/userInput.ts
+++ back/src/interfaces/userInput.ts
@@ -8,6 +8,12 @@ export interface IUserInput {
   job?: string;
 }
 
+export interface IUserInfoUpdateInput {
+  gender: string;
+  age_range: string;
+  job: string;
+}
+
 export interface IUserModelInput {
   pk_user_id: string;
   user_name: string;
