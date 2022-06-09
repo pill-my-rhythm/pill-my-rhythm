fa69928c502d5f4c89b9cf2aec47f4a9afcf4ecc
parkjungyoon <tbr06057@naver.com>
2022-06-10 01:49:56 +0900

    [BE] refact: app.ts

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,18 +1,20 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import { errorMiddleware } from "./middlewares/errorMiddleware";
+
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 import path from "path";
+
 import { UserRouter } from "./routers/userRouter";
+import { errorMiddleware } from "./middlewares/errorMiddleware";
 
 const app: express.Application = express();
 const swaggerSpec = YAML.load(path.join(__dirname, "./swagger.yaml"));
+
 // swagger
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
