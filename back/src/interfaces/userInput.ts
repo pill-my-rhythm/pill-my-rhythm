7a3b4c5f5d1161eebad25a8ae3a2cf16db1c2458
parkjungyoon <tbr06057@naver.com>
2022-06-09 14:34:50 +0900

    [BE] modify: 필요없는 인터페이스 삭제

--- back/src/interfaces/userInput.ts
+++ back/src/interfaces/userInput.ts
@@ -7,13 +7,3 @@ export interface IUserInput {
   age_range?: string;
   job?: string;
 }
-
-export interface IUserModelInput {
-  pk_user_id: string;
-  user_name: string;
-  email: string;
-  password: string;
-  gender?: string;
-  age_range?: string;
-  job?: string;
-}
