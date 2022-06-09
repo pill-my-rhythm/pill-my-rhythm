a8328aa62f54fbcd9412916f0f3564387a7be14d
SongheeNoh <s0n9h2@gmail.com>
2022-06-09 21:27:33 +0900

    [BE] feat: schedule.ts 모델 추가

---
+++ back/src/db/models/schedule.ts
@@ -0,0 +1,60 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "./index";
+import { Users } from "./user";
+
+// These are all the attributes in the User model
+
+interface SchedulesAttributes {
+  pk_schedule_id?: number;
+  //   fk_user_id: string;
+  type: string;
+  when: Date;
+  to_do: string;
+}
+
+export class Schedules extends Model<SchedulesAttributes> {
+  pk_schedule_id?: number;
+  //   fk_user_id: string;
+  type: string;
+  when: Date;
+  to_do: string;
+}
+
+Schedules.init(
+  {
+    pk_schedule_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    // fk_user_id: {
+    //   type: DataTypes.CHAR(36),
+    //   allowNull: false,
+    // },
+    type: {
+      type: DataTypes.ENUM("B", "S"),
+      allowNull: false,
+    },
+    when: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    to_do: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    modelName: "Schedules",
+    tableName: "tb_schedule",
+    freezeTableName: true,
+    timestamps: true,
+    paranoid: true, // 삭제일 (복구용)
+    underscored: true,
+  },
+);
+
+Users.hasMany(Schedules, { foreignKey: { name: "fk_user_id", allowNull: false } });
+Schedules.belongsTo(Users, { foreignKey: { name: "fk_user_id", allowNull: false } });
