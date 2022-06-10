ffd77f8b4b29ebd004256886f5218e84511feba0
SongheeNoh <s0n9h2@gmail.com>
2022-06-11 03:11:53 +0900

    [BE] feat: addSchedule 메소드 구현

---
+++ back/src/services/scheduleService.ts
@@ -0,0 +1,18 @@
+import { Schedule } from "../db/Schedule";
+// import { Users } from "../db/models/user";
+import { IScheduleCreateInput } from "../interfaces/scheduleInput";
+
+const ScheduleService = {
+  addSchedule: async (fk_user_id: string, data: IScheduleCreateInput) => {
+    const schedule = await Schedule.findByTime(fk_user_id, data.start, data.finish);
+    console.log(schedule);
+    if (schedule) {
+      throw new Error("선택한 시간에 다른 일정이 있습니다.");
+    }
+    data.fk_user_id = fk_user_id;
+    const newSchedule = await Schedule.createSchedule(data);
+    return newSchedule;
+  },
+};
+
+export { ScheduleService };
