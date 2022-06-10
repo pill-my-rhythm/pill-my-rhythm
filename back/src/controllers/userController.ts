146b3956a17d9e75f50a56cd0b44eb5863362561
parkjungyoon <tbr06057@naver.com>
2022-06-10 14:31:15 +0900

    [BE] refact: userController 로직 분리

---
+++ back/src/controllers/userController.ts
@@ -0,0 +1,54 @@
+import { Request, Response, NextFunction } from "express";
+import { UserService } from "../services/userService";
+import { Users } from "../db/models/user";
+import { IUserInput, IUserLoginInput, IUserInfoUpdateInput } from "../interfaces/userInput";
+
+const userController = {
+  register: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { user_name, email, password, gender, age_range, job }: IUserInput = req.body;
+      const newUser = await UserService.addUser({ user_name, email, password, gender, age_range, job });
+
+      res.status(201).json(newUser);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  login: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { email, password }: IUserLoginInput = req.body;
+      const user = await UserService.getUser(email, password);
+
+      res.status(200).json(user);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  updateInfo: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const pk_user_id: Users["pk_user_id"] = req.currentUserId;
+      const { password, gender, age_range, job }: IUserInfoUpdateInput = req.body;
+      const updateDate = { password, gender, age_range, job };
+
+      const result = await UserService.updateUserInfo(pk_user_id, updateDate);
+      res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  withdrawal: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const pk_user_id = req.currentUserId;
+      const deletedUser = await UserService.delete(pk_user_id);
+
+      res.status(201).json(deletedUser);
+    } catch (error) {
+      next(error);
+    }
+  },
+};
+
+export { userController };
