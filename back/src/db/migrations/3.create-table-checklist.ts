e39891f05be103d0113629202a875d4471303536
SongheeNoh <s0n9h2@gmail.com>
2022-06-10 09:56:27 +0900

    [BE] modify: 변수명 변경 적용

--- back/src/db/migrations/3.create-table-checklist.ts
+++ back/src/db/migrations/3.create-table-checklist.ts
@@ -2,11 +2,11 @@
 // 실행시키면 table 생성됨
 // 실행시킬 수 없다고 나올 땐 https://cobee.tistory.com/entry/TypeScript%EC%98%A4%EB%A5%98-tsc-%EC%9D%B4-%EC%8B%9C%EC%8A%A4%ED%85%9C%EC%97%90%EC%84%9C-%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8%EB%A5%BC-%EC%8B%A4%ED%96%89%ED%95%A0-%EC%88%98-%EC%97%86%EC%9C%BC%EB%AF%80%EB%A1%9C-%EB%B3%B4%EC%95%88-%EC%98%A4%EB%A5%98
 
-import { Checklist } from "../models/checklist";
+import { Checklists } from "../models/checklist";
 
 console.log("======Create checklist Table======");
 const create_table_checklist = async () => {
-  await Checklist.sync({ force: true })
+  await Checklists.sync({ force: true })
     .then(() => {
       console.log("✅Success Create checklist Table");
     })
