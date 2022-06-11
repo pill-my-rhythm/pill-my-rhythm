392a82bf263647c1495db7bef8eb68fd540e07d4
YounJoo Seog <reina.seog@gmail.com>
2022-06-11 18:22:04 +0900

    [FE] Modify : 간략 주석 추가

--- front/src/routes/PR/Result/RecommendationArea.tsx
+++ front/src/routes/PR/Result/RecommendationArea.tsx
@@ -5,8 +5,9 @@ const RecommendationArea = () => {
   return (
     <div className="min-h-screen bg-base-200">
       <div>
-        <h2>다음에</h2>
+        <h2>페이지 내용 제목</h2>
       </div>
+      {/* // * 나중에 값 받아서 map으로 돌려서 뿌려줄 예정! 일단 5개를 예상해서 형태만 잡아둠 => PR카드로 컴포넌트 했는데 값 받아오면 컴포넌트화는 다시 원위치할 듯 */}
       <div className="flex flex-row">
         <PRCard />
         <PRCard />
