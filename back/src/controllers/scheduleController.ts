47c5c0fdf3822704cb7f8f0413e93af990e34ce4
parkjungyoon <tbr06057@naver.com>
2022-06-11 17:36:30 +0900

    [BE] modify: 불필요한 주석 제거

--- back/src/controllers/scheduleController.ts
+++ back/src/controllers/scheduleController.ts
@@ -6,8 +6,6 @@ import { IScheduleCreateInput } from "../interfaces/scheduleInput";
 const ScheduleController = {
   getAllSchedule: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      //   const { user_name, email, password, gender, age_range, job }: IUserInput = req.body;
-      //   const newUser = await UserService.addUser({ user_name, email, password, gender, age_range, job });
       const pk_user_id: Users["pk_user_id"] = req.currentUserId;
       const result = await ScheduleService.getAllSchedule(pk_user_id);
       res.status(201).json(result);
