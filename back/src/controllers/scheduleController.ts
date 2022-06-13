8430c4e9c2718bd1cfd72e417ec4edb006d46ec0
parkjungyoon <tbr06057@naver.com>
2022-06-14 00:35:32 +0900

    [BE] add: 스케줄 조회, body 추가

--- back/src/controllers/scheduleController.ts
+++ back/src/controllers/scheduleController.ts
@@ -1,13 +1,14 @@
 import { Request, Response, NextFunction } from "express";
 import { ScheduleService } from "../services/scheduleService";
-import { Users } from "../db/models/user";
 import { IScheduleCreateInput } from "../interfaces/scheduleInput";
 
 const ScheduleController = {
-  getAllSchedule: async (req: Request, res: Response, next: NextFunction) => {
+  getWeeklySchedule: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const pk_user_id: Users["pk_user_id"] = req.currentUserId;
-      const result = await ScheduleService.getAllSchedule(pk_user_id);
+      const fk_user_id: string = req.currentUserId;
+      const start: Date = new Date(req.body.start);
+      const finish: Date = new Date(req.body.finish);
+      const result = await ScheduleService.getWeeklySchedule(fk_user_id, { start, finish });
       res.status(201).json(result);
     } catch (error) {
       next(error);
