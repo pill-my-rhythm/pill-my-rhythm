fa97978c39ac9f882e2fa28086839de04c242009
SongheeNoh <s0n9h2@gmail.com>
2022-06-14 01:36:41 +0900

    [BE] add: 테스트용 weekly 컨트롤러 생성

--- back/src/controllers/checklistController.ts
+++ back/src/controllers/checklistController.ts
@@ -1,16 +1,27 @@
 import { Request, Response, NextFunction } from "express";
 import { ChecklistService } from "../services/checklistService";
-import { IChecklistCreateInput } from "../interfaces/checklistInput";
+import { IChecklistCreateInput, IChecklistWeeklyInput } from "../interfaces/checklistInput";
 
 const ChecklistController = {
   create: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const fk_user_id: string = req.currentUserId;
-      const date: string = req.body.date.slice(0, 10);
-      const { one, two, three, four, five, six }: IChecklistCreateInput = req.body;
+      const { date, one, two, three, four, five, six }: IChecklistCreateInput = req.body;
       const newChecklist = await ChecklistService.addChecklist({ fk_user_id, date, one, two, three, four, five, six });
 
       res.status(201).json(newChecklist);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  getWeekly: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const fk_user_id: string = req.currentUserId;
+      const { start, finish }: IChecklistWeeklyInput = req.body;
+      const weeklyChecklist = await ChecklistService.getWeeklyChecklist(fk_user_id, { start, finish });
+
+      res.status(200).json(weeklyChecklist);
     } catch (error) {
       next(error);
     }
