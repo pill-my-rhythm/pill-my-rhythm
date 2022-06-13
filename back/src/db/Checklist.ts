d560fcb74b5ac96f0bd78a0d1265c3c8cc7ea885
parkjungyoon <tbr06057@naver.com>
2022-06-14 00:36:05 +0900

    [BE] modify: 함수 명 변경

--- back/src/db/Checklist.ts
+++ back/src/db/Checklist.ts
@@ -3,8 +3,8 @@ import { Users } from "./models/user";
 import { Checklist } from "./models/checklist";
 
 const Checklist_ = {
-  findById: async (pk_user_id: string) => {
-    const schedule = await Checklist.findAll({ include: { model: Users, where: { pk_user_id: pk_user_id } } });
+  findById: async (fk_user_id: string) => {
+    const schedule = await Checklist.findAll({ include: { model: Users, where: { pk_user_id: fk_user_id } } });
     return schedule;
   },
 };
--- back/src/db/DailySupplement.ts
+++ back/src/db/DailySupplement.ts
@@ -3,8 +3,8 @@ import { Users } from "./models/user";
 import { DailySupplements } from "./models/dailySupplement";
 
 const DailySupplement = {
-  findById: async (pk_user_id: string) => {
-    const schedule = await DailySupplements.findAll({ include: { model: Users, where: { pk_user_id: pk_user_id } } });
+  findById: async (fk_user_id: string) => {
+    const schedule = await DailySupplements.findOne({ include: { model: Users, where: { pk_user_id: fk_user_id } } });
     return schedule;
   },
 };
