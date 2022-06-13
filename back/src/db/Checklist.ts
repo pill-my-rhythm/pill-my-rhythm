fb5cd39aa28f36845b098b478702065fb784a0bf
SongheeNoh <s0n9h2@gmail.com>
2022-06-14 01:43:40 +0900

    [BE] feat: weekly 체크리스트 조회 기능 구현 완료

--- back/src/db/Checklist.ts
+++ back/src/db/Checklist.ts
@@ -1,6 +1,7 @@
+import { Op } from "./models";
 import { Checklists } from "./models/checklist";
 import { Users } from "./models/user";
-import { IChecklistCreateType } from "../interfaces/checklistInput";
+import { IChecklistCreateType, IChecklistWeeklyInput } from "../interfaces/checklistInput";
 
 const Checklist = {
   createChecklist: async (newChecklistData: IChecklistCreateType) => {
@@ -8,12 +9,22 @@ const Checklist = {
     return checklist;
   },
 
-  findByDate: async (fk_user_id: string, date: string) => {
+  findByDate: async (fk_user_id: string, date: Date) => {
     const checklist = await Checklists.findOne({
       where: { date },
       include: { model: Users, where: { pk_user_id: fk_user_id } },
     });
     return checklist;
+  },
+
+  findByWeek: async (fk_user_id: string, { start, finish }: IChecklistWeeklyInput) => {
+    const checklists = await Checklists.findAll({
+      attributes: ["date", "level"],
+      where: { date: { [Op.between]: [start, finish] } },
+      include: { model: Users, attributes: [], where: { pk_user_id: fk_user_id } },
+      order: [["date", "ASC"]],
+    });
+    return checklists;
   },
 };
 
