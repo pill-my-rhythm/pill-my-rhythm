7822b66dcac6241ba3c8d65434f5ca94ca5e1bd5
YounJoo Seog <reina.seog@gmail.com>
2022-06-14 19:58:26 +0900

    [FE] feart : reducer 추가

---
+++ front/src/reducer.ts
@@ -0,0 +1,18 @@
+export function loginReducer(userState: any, action: { type: any; payload: any }) {
+  switch (action.type) {
+    case "LOGIN_SUCCESS":
+      console.log("%c로그인!", "color: #d93d1a;");
+      return {
+        ...userState,
+        user: action.payload,
+      };
+    case "LOGOUT":
+      console.log("%c로그아웃!", "color: #d93d1a;");
+      return {
+        ...userState,
+        user: null,
+      };
+    default:
+      return userState;
+  }
+}
