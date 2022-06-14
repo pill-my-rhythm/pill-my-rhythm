bbffbc62cb9046b08696e8fe83a978b170c92114
parkjungyoon <tbr06057@naver.com>
2022-06-14 18:46:44 +0900

    [BE] feat: verify token middleware

---
+++ back/src/middlewares/verifyToken.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    if (!req.headers["authorization"]) {
+      res.status(401).json({
+        status: "fail",
+        message: "token이 없습니다",
+      });
+    } else {
+      const secretKey: string = process.env.JWT_SECRET_KEY;
+      const userToken = req.headers["authorization"].split(" ")[1];
+      const jwtDecoded: any = jwt.verify(userToken, secretKey);
+      req.currentUserId = jwtDecoded.user_id;
+      next();
+    }
+  } catch (error) {
+    res.status(401).json({
+      status: "fail",
+      message: "token이 변형되었습니다. ",
+      error,
+    });
+    next(error);
+  }
+};
+
+export { verifyToken };
