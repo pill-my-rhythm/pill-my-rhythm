bfba41cfdac0a8947b38b4a9988d1fdb29498003
YounJoo Seog <reina.seog@gmail.com>
2022-06-14 22:07:12 +0900

    [FE] Add: Login/Register Router 추가

--- front/src/Router.tsx
+++ front/src/Router.tsx
@@ -2,14 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./routes/PR/Main";
 import Header from "./routes/_shared/Header";
 import Schedule from "./routes/Schedule/Schedule";
+import Register from "./routes/User/Reigsterform";
+import Login from "./routes/User/Login";
 
 function Router() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
+        <Route path="/register" element={<Register />} />
         <Route path="/" element={<Main />} />
         <Route path="/schedule" element={<Schedule />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
