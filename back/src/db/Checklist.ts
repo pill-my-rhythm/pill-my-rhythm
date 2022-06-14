5d3bcbaa7f9f4168aa461197bcb2cd242cb98a61
parkjungyoon <tbr06057@naver.com>
2022-06-14 12:02:43 +0900

    [BE] feat: checklist 조회 함수

--- back/src/db/Checklist.ts
+++ back/src/db/Checklist.ts
@@ -1,11 +1,16 @@
-// import { Op } from "./models";
+import { Op } from "./models";
 import { Users } from "./models/user";
 import { Checklist } from "./models/checklist";
 
 const Checklist_ = {
-  findById: async (fk_user_id: string) => {
-    const schedule = await Checklist.findAll({ include: { model: Users, where: { pk_user_id: fk_user_id } } });
-    return schedule;
+  findByWeek: async (pk_user_id: string, start: Date, finish: Date) => {
+    const checklists = await Checklist.findAll({
+      attributes: ["date", "level"],
+      where: { date: { [Op.between]: [start, finish] } },
+      include: { model: Users, attributes: [], where: { pk_user_id: pk_user_id } },
+      order: [["date", "ASC"]],
+    });
+    return checklists;
   },
 };
 
