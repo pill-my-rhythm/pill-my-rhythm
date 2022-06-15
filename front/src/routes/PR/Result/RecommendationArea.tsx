eca213d9d54be9e9867e52f026cf7b62dda74d76
YounJoo Seog <reina.seog@gmail.com>
2022-06-16 00:37:10 +0900

    [FE] style: 유투브 화면 노출 css 수정

--- front/src/routes/PR/Result/RecommendationArea.tsx
+++ front/src/routes/PR/Result/RecommendationArea.tsx
@@ -8,7 +8,7 @@ const RecommendationArea = () => {
         <h2>페이지 내용 제목</h2>
       </div>
       {/* // * 나중에 값 받아서 map으로 돌려서 뿌려줄 예정! 일단 5개를 예상해서 형태만 잡아둠 => PR카드로 컴포넌트 했는데 값 받아오면 컴포넌트화는 다시 원위치할 듯 */}
-      <div className="flex flex-row">
+      <div className="flex">
         <PRCard />
         <PRCard />
         <PRCard />
