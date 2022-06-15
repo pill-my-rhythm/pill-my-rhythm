118c73b3babeb2d85c8ee510fea426e3df6ec99d
YounJoo Seog <reina.seog@gmail.com>
2022-06-15 16:26:43 +0900

    [FE] Modify : Dispatcher 수정중

--- front/src/Dispatcher.tsx
+++ front/src/Dispatcher.tsx
@@ -16,34 +16,33 @@ function Dispatcher() {
   // 아래 코드를 보면 isFetchCompleted 가 true여야 컴포넌트가 구현됨.
   const [isFetchCompleted, setIsFetchCompleted] = useState(false);
 
-  const fetchCurrentUser = React.useCallback(async () => {
+  const fetchCurrentUser = async () => {
     try {
       // 이전에 발급받은 토큰이 있다면, 이를 가지고 유저 정보를 받아옴.
       const user = sessionStorage.getItem("userToken");
 
-      const res = await get("users");
+      const res = await get("user/current");
       const currentUser = res.data;
+
       // dispatch 함수를 통해 로그인 성공 상태로 만듦.
       dispatch({
         type: "LOGIN_SUCCESS",
         payload: currentUser,
       });
-
-      console.log("%c sessionStorage에 토큰 있음.", "color: #d93d1a;");
     } catch {
       console.log("%c SessionStorage에 토큰 없음.", "color: #d93d1a;");
     }
     // fetchCurrentUser 과정이 끝났으므로, isFetchCompleted 상태를 true로 바꿔줌
     setIsFetchCompleted(true);
-  }, []);
+  };
 
   // useEffect함수를 통해 fetchCurrentUser 함수를 실행함.
   useEffect(() => {
     fetchCurrentUser();
-  }, [fetchCurrentUser]);
+  }, []);
 
   if (!isFetchCompleted) {
-    return <span>loading...</span>;
+    return <span> Loading..</span>;
   }
 
   return (
