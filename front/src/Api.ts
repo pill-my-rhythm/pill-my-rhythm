67dbed5889d57d7d4cab03c5e046933482f2894d
YounJoo Seog <reina.seog@gmail.com>
2022-06-15 20:37:55 +0900

    [FE] Fix : api type 변경

--- front/src/Api.ts
+++ front/src/Api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const backendPortNumber = "5000";
 const serverUrl = window.location.protocol + "//" + window.location.hostname + ":" + backendPortNumber + "/";
-async function get(endpoint: any, params = "") {
+async function get(endpoint: string, params = "") {
   return axios.get(serverUrl + endpoint + params, {
     // JWT 토큰을 헤더에 담아 백엔드 서버에 보냄.
     headers: {
@@ -11,7 +11,7 @@ async function get(endpoint: any, params = "") {
   });
 }
 
-async function post(endpoint: any, data: any) {
+async function post(endpoint: string, data: any) {
   // JSON.stringify 함수: Javascript 객체를 JSON 형태로 변환함.
   // 예시: {name: "Kim"} => {"name": "Kim"}
   const bodyData = JSON.stringify(data);
@@ -23,7 +23,7 @@ async function post(endpoint: any, data: any) {
   });
 }
 
-async function put(endpoint: any, data: any) {
+async function put(endpoint: string, data: any) {
   // JSON.stringify 함수: Javascript 객체를 JSON 형태로 변환함.
   // 예시: {name: "Kim"} => {"name": "Kim"}
   const bodyData = JSON.stringify(data);
@@ -37,7 +37,7 @@ async function put(endpoint: any, data: any) {
 
 // 아래 함수명에 관해, delete 단어는 자바스크립트의 reserved 단어이기에,
 // 여기서는 우선 delete 대신 del로 쓰고 아래 export 시에 delete로 alias 함.
-async function del(endpoint: any, params = "") {
+async function del(endpoint: string, params = "") {
   return axios.delete(serverUrl + endpoint + "/" + params, {
     headers: {
       Authorization: `Bearer ${sessionStorage.getItem("userToken")}`,
