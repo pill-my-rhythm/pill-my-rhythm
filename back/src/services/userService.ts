89880908df1c15c87eff412f4ca5ac1bf9f5a30a
parkjungyoon <tbr06057@naver.com>
2022-06-15 13:46:40 +0900

    [BE] feat: 로그인 로직 수정+refresh 토큰 발급

--- back/src/services/userService.ts
+++ back/src/services/userService.ts
@@ -2,6 +2,7 @@ import { User } from "../db/User";
 import { Users } from "../db/models/user";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { makeToken, makeRefreshToken } from "../utils/jwt-util";
 import { IUserInput, IUserInfoUpdateInput } from "../interfaces/userInput";
 
 const UserService = {
@@ -18,40 +19,32 @@ const UserService = {
     return newUser;
   },
 
-  // getUser: async ({ email, password }: loginType) => {
   getUser: async (email: string, password: string) => {
-    // 이메일 db에 존재 여부 확인
     const user = await User.findByEmail(email);
     if (!user) {
       throw new Error("해당 이메일은 가입 내역이 없습니다. 다시 한 번 확인해 주세요.");
     }
-
-    // 비밀번호 일치 여부 확인
     const correctPasswordHash: string = user.password;
     const isPasswordCorrect: boolean = await bcrypt.compare(password, correctPasswordHash);
     if (!isPasswordCorrect) {
       throw new Error("비밀번호가 일치하지 않습니다. 다시 한 번 확인해 주세요.");
     }
 
-    // 로그인 성공 -> JWT 웹 토큰 생성
-    const secretKey: string = process.env.JWT_SECRET_KEY || "jwt-secret-key";
-    const token = jwt.sign({ user_id: user.pk_user_id }, secretKey);
+    // 로그인 성공 -> access token, refresh token 발급
+    const accessToken = makeToken({ userId: user.pk_user_id });
+    const refreshToken = makeRefreshToken();
 
-    // 반환할 loginuser 객체를 위한 변수 설정
     const { pk_user_id, user_name, gender, age_range, job } = user;
-
-    const loginUser = {
-      token,
+    const discoveredUser = {
       pk_user_id,
       user_name,
       email,
-      password,
       gender,
       age_range,
       job,
     };
 
-    return loginUser;
+    return { discoveredUser, accessToken, refreshToken };
   },
 
   updateUserInfo: async (pk_user_id: string, updateDate: IUserInfoUpdateInput) => {
