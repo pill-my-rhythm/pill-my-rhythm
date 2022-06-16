ba89900f22a21535f6c074d117cff1ecb9cd68e3
YounJoo Seog <reina.seog@gmail.com>
2022-06-16 18:56:17 +0900

    [FE} style : 결과 영양제 페이지 수정중

---
+++ front/src/routes/PR/Result/MockupData.js
@@ -0,0 +1,13 @@
+const MockupData = {
+  id: "28",
+  name: "리체 클리어아이 루테인",
+  caution: "과다 섭취 시 일시적으로 피부가 황색으로 변할 수 있음",
+  company: "(주)HK바이오텍",
+  functuion: "노화로 인해 감소될 수 있는 황반색소밀도를 유지하여 눈 건강에 도움을 줄 수 있음",
+  how_to_eat: "1일 1회, 1회 1캡슐을 식사 후 충분한 물과 함께 섭취하십시오.",
+  raw: "마리골드꽃추출물(고시형),오징어먹물색소,카라멜색소,이산화티타늄,정제수,글리세린,젤라틴,홍화유",
+  img: "https://shopping-phinf.pstatic.net/main_2928270/29282707435.jpg",
+  shop: `https://search.shopping.naver.com/product/29282707435`,
+};
+
+export default MockupData;
