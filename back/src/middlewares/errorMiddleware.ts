64b02e918bf03fd28019c1b1bfab40e632b3319f
parkjungyoon <tbr06057@naver.com>
2022-06-16 16:25:06 +0900

    [BE] refact: Express Error handling middleware

--- back/src/middlewares/errorMiddleware.ts
+++ back/src/middlewares/errorMiddleware.ts
@@ -1,10 +1,12 @@
 import { Request, Response, NextFunction } from "express";
+import { HttpException } from "../utils/error-util";
 
-function errorMiddleware(error: Error, req: Request, res: Response, next: NextFunction) {
+function errorMiddleware(error: HttpException, req: Request, res: Response, next: NextFunction) {
   // 터미널에 노란색으로 출력됨.
   console.log("\x1b[33m%s\x1b[0m", error);
+  const status = error.status || 500;
   res.json({
-    status: res.statusCode,
+    status: status,
     message: error.message,
   });
 }
