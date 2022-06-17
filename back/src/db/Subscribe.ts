f81bf69263f69b598f2ead6864438597953e1ded
SongheeNoh <s0n9h2@gmail.com>
2022-06-17 23:06:00 +0900

    [BE] add: push-test를 위한 메소드 추가

--- back/src/db/Subscribe.ts
+++ back/src/db/Subscribe.ts
@@ -1,14 +1,21 @@
 import { Subscribes } from "./models/subscribe";
+import { ISendNotificationInput } from "../interfaces/subscribeInput";
 
 const Subscribe = {
-  findByUserId: async (fk_user_id: string, device_token: JSON) => {
+  findByUserAndDevice: async (fk_user_id: string, device_token: ISendNotificationInput) => {
     const subscription = await Subscribes.findOne({ where: { fk_user_id, device_token } });
+    console.log(subscription);
     return subscription;
   },
 
-  create: async (fk_user_id: string, device_token: JSON) => {
+  create: async (fk_user_id: string, device_token: ISendNotificationInput) => {
     const newSubscription = await Subscribes.create({ fk_user_id, device_token });
     return newSubscription;
+  },
+
+  findByUserId: async (fk_user_id: string) => {
+    const devicesArray = await Subscribes.findAll({ where: { fk_user_id } });
+    return devicesArray;
   },
 };
 
