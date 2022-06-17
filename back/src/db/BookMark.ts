28ab6c7a179d9bd1c928fe8e5297d03c381e923b
parkjungyoon <tbr06057@naver.com>
2022-06-17 22:08:12 +0900

    [BE] add: createBookMark 함수 생성

--- back/src/db/BookMark.ts
+++ back/src/db/BookMark.ts
@@ -1,16 +1,17 @@
 import { Op } from "./models";
 import { BookMarks } from "./models/bookMark";
 import { Users } from "./models/user";
+import { IBookMarkCreateInput } from "../interfaces/bookMarkInput";
 
 const BookMark = {
   findById: async (fk_user_id: string) => {
     const schedule = await BookMarks.findAll({ include: { model: Users, where: { pk_user_id: fk_user_id } } });
     return schedule;
   },
-  //   createBookMark: async () => {
-  //     const bookmark = await BookMarks.create(bookmark);
-  //     return bookmarks;
-  //   },
+  createBookMark: async (data: IBookMarkCreateInput) => {
+    const newBookMark = await BookMarks.create(data);
+    return newBookMark;
+  },
 };
 
 export { BookMark };
