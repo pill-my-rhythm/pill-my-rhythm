96aa60588abcfac76a22f6c9925c2c417c921ac9
SongheeNoh <s0n9h2@gmail.com>
2022-06-17 23:08:24 +0900

    [BE] add: push-test 기능 서비스 계층 구현

--- back/src/services/subscribeService.ts
+++ back/src/services/subscribeService.ts
@@ -1,15 +1,25 @@
 import { Subscribe } from "../db/Subscribe";
 import { HttpException } from "../utils/error-util";
+import { ISendNotificationInput } from "../interfaces/subscribeInput";
+import webPush from "../utils/webPush";
 
 const SubscribeService = {
-  createSubscription: async (fk_user_id: string, device_token: JSON) => {
-    const subscription = await Subscribe.findByUserId(fk_user_id, device_token);
+  createSubscription: async (fk_user_id: string, device_token: ISendNotificationInput) => {
+    const subscription = await Subscribe.findByUserAndDevice(fk_user_id, device_token);
     if (subscription) {
       throw new HttpException(401, "이미 구독 신청을 한 기기입니다.");
     }
 
     const newSubscription = await Subscribe.create(fk_user_id, device_token);
     return newSubscription;
+  },
+
+  sendPushNotification: async (fk_user_id: string) => {
+    const devicesArray = await Subscribe.findByUserId(fk_user_id);
+    devicesArray.forEach((element) => {
+      console.log(element.getDataValue("device_token"));
+      webPush(element.getDataValue("device_token"));
+    });
   },
 };
 
