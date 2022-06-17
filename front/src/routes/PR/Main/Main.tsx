6d5d518985e08a9bb0588bc7e6b2b3fcee0c585f
YounJoo Seog <reina.seog@gmail.com>
2022-06-17 20:13:31 +0900

    [FE] fix : console창 error 수정

--- front/src/routes/PR/Main/Main.tsx
+++ front/src/routes/PR/Main/Main.tsx
@@ -41,7 +41,7 @@ const Main = () => {
                 <input type="text" placeholder="요즘 눈이 안 좋아요... 면역력이 떨어졌어요..." className="input input-bordered w-5/6" />
                 <button className="btn btn-primary" onClick={tempNavigate}>
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                   </svg>
                 </button>
               </div>
