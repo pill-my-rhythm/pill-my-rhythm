08109b5306ec0395440a2c3e97538b0fd5c57f28
parkjungyoon <tbr06057@naver.com>
2022-06-18 01:06:37 +0900

    [BE] feat: deletedDailySchedule 메소드

--- back/src/db/DailySupplement.ts
+++ back/src/db/DailySupplement.ts
@@ -1,15 +1,30 @@
 // import { Op } from "./models";
 import { Users } from "./models/user";
+import { Supplements } from "./models/supplement";
 import { DailySupplements } from "./models/dailySupplement";
 import { IDailySupplementCreateInput } from "../interfaces/scheduleInput";
 
 const DailySupplement = {
   findById: async (fk_user_id: string) => {
-    const schedule = await DailySupplements.findOne({ include: { model: Users, where: { pk_user_id: fk_user_id } } });
+    const schedule = await DailySupplements.findAll({
+      include: [
+        { model: Users, attributes: [], where: { pk_user_id: fk_user_id } },
+        { model: Supplements, attributes: ["name"] },
+      ],
+    });
     return schedule;
   },
   createDailySchedule: async (data: IDailySupplementCreateInput) => {
     const dailySchedule = await DailySupplements.create(data);
+    return dailySchedule;
+  },
+  deletedDailySchedule: async (fk_user_id: string, pk_plan_id: number) => {
+    const dailySchedule = await DailySupplements.destroy({
+      where: {
+        pk_plan_id: pk_plan_id,
+      },
+      force: true,
+    });
     return dailySchedule;
   },
 };
