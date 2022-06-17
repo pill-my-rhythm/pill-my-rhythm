1e8881ba691cc8d747b0498cc1f171d77ca529ba
SongheeNoh <s0n9h2@gmail.com>
2022-06-17 22:53:24 +0900

    [BE] add: device_token type 변경, test-push 기능 컨트롤러 부분 구현

--- back/src/controllers/subscribeController.ts
+++ back/src/controllers/subscribeController.ts
@@ -1,14 +1,25 @@
 import { Request, Response, NextFunction } from "express";
 import { SubscribeService } from "../services/subscribeService";
+import { ISendNotificationInput } from "../interfaces/subscribeInput";
 
 const SubscribeController = {
   subscribeNotification: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const fk_user_id: string = req.currentUserId;
-      const device_token: JSON = req.body.device_token;
+      const device_token: ISendNotificationInput = req.body.device_token;
 
       const subscription = await SubscribeService.createSubscription(fk_user_id, device_token);
       return subscription;
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  pushNotification: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const fk_user_id: string = req.currentUserId;
+      const push = await SubscribeService.sendPushNotification(fk_user_id);
+      return push;
     } catch (error) {
       next(error);
     }
