e8e07bcb3cf39700201167cabe4687d92f6b6b41
SongheeNoh <s0n9h2@gmail.com>
2022-06-17 23:10:30 +0900

    [FE] add: 서비스 워커 메세지 전송 기능 추가 (일단은 public에)

---
+++ front/public/sw.js
@@ -0,0 +1,20 @@
+window.self.addEventListener("push", (e) => {
+  const options = {
+    body: "This notification was generated from a push!",
+    icon: "images/example.png",
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: "2",
+    },
+    actions: [
+      {
+        action: "explore",
+        title: "Explore this new world",
+        icon: "images/checkmark.png",
+      },
+      { action: "close", title: "Close", icon: "images/xmark.png" },
+    ],
+  };
+  window.self.registration.showNotification("Hello world!", options);
+});
