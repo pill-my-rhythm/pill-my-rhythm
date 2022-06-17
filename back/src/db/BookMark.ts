cd3cc208d157af016e64c6a3631461bf746b503b
parkjungyoon <tbr06057@naver.com>
2022-06-17 23:25:11 +0900

    [BE] 북마크 조회 API 메소드 생성

--- back/src/db/BookMark.ts
+++ back/src/db/BookMark.ts
@@ -1,11 +1,32 @@
 import { Op } from "./models";
-import { BookMarks } from "./models/bookMark";
 import { Users } from "./models/user";
+import { BookMarks } from "./models/bookMark";
+import { Supplements } from "./models/supplement";
 import { IBookMarkCreateInput } from "../interfaces/bookMarkInput";
 
 const BookMark = {
   findById: async (fk_user_id: string) => {
-    const schedule = await BookMarks.findAll({ include: { model: Users, where: { pk_user_id: fk_user_id } } });
+    const schedule = await BookMarks.findAll({
+      include: [
+        { model: Users, attributes: [], where: { pk_user_id: fk_user_id } },
+        {
+          model: Supplements,
+          attributes: [
+            "pk_supplement_id",
+            "update_date",
+            "shape",
+            "name",
+            "caution",
+            "company",
+            "function",
+            "how_to_eat",
+            "raw",
+            "img_link",
+            "link",
+          ],
+        },
+      ],
+    });
     return schedule;
   },
   createBookMark: async (data: IBookMarkCreateInput) => {
