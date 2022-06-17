6cdee4866a0600ae74c3cf245af4f10062a5c4b9
parkjungyoon <tbr06057@naver.com>
2022-06-17 23:25:38 +0900

    [BE] add: 북마크 service layer

--- back/src/services/bookMarkService.ts
+++ back/src/services/bookMarkService.ts
@@ -1,5 +1,7 @@
+import { User } from "../db/User";
 import { BookMark } from "../db/BookMark";
 import { HttpException } from "../utils/error-util";
+import { IBookMarkCreateInput } from "../interfaces/bookMarkInput";
 
 const BookMarkService = {
   getAllBookmarks: async (fk_user_id: string) => {
@@ -8,6 +10,15 @@ const BookMarkService = {
       throw new HttpException(401, "북마크 내역이 없습니다.");
     }
     return bookmarks;
+  },
+
+  addBookMark: async (data: IBookMarkCreateInput) => {
+    const user = await User.findById(data.fk_user_id);
+    if (!user) {
+      throw new HttpException(401, "가입 내역이 없는 계정입니다. 다시 한 번 확인해 주세요.");
+    }
+    const newBookMark = await BookMark.createBookMark(data);
+    return newBookMark;
   },
 };
 
