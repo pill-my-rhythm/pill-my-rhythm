113d8b24af485e0f504af528f78a2b77fe5de28e
YounJoo Seog <reina.seog@gmail.com>
2022-06-18 22:41:22 +0900

    [FE] Modify: 일단 백업

--- front/src/routes/PR/Result/Result.tsx
+++ front/src/routes/PR/Result/Result.tsx
@@ -1,13 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import VideoArea from "./VideoArea";
 import RecommendationArea from "./RecommendationArea";
 import Promotion from "./Promotion";
 import { UserStateContext } from "../../../Dispatcher";
 
+// export interface UserData {
+//   age_range: string;
+//   createdAt: Date;
+//   deletedAt: null;
+//   email: string;
+//   gender: string;
+//   job: string;
+//   password: string;
+//   pk_user_id: string;
+//   updatedAt: Date;
+//   user_name: string;
+// }
+
 const Result = () => {
   const userState = useContext(UserStateContext);
   const isLogin = !!userState.user;
-  // const name = userState.user.userInfo.user_name;
+  const user = userState.user;
+
+  console.log("#user", user);
 
   return (
     <div className="overflow-scroll:hidden">
