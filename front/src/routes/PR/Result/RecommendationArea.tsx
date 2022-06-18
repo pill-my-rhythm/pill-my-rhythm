61f07b8b2cb72f17bc3083cb003272ecc6d38637
YounJoo Seog <reina.seog@gmail.com>
2022-06-18 17:44:35 +0900

    [FE] Add: 결과 페이지 카드 하드코딩한 거 수정

--- front/src/routes/PR/Result/RecommendationArea.tsx
+++ front/src/routes/PR/Result/RecommendationArea.tsx
@@ -1,9 +1,6 @@
 import React, { useContext, useState } from "react";
-import PRCard1 from "./PRCard1";
-import PRCard2 from "./PRCard2";
-import PRCard3 from "./PRCard3";
-import PRCard4 from "./PRCard4";
-import PRCard5 from "./PRCard5";
+import PRCard from "./PRCard";
+
 import { UserStateContext } from "../../../Dispatcher";
 
 const RecommendationArea = () => {
@@ -23,13 +20,8 @@ const RecommendationArea = () => {
           <h2>당신에게는 이런 영양제를 추천드려요!</h2>
           {/* } */}
         </div>
-        {/* // * 나중에 값 받아서 map으로 돌려서 뿌려줄 예정! 일단 5개를 예상해서 형태만 잡아둠 => PR카드로 컴포넌트 했는데 값 받아오면 컴포넌트화는 다시 원위치할 듯 */}
         <div className="flex flex-wrap items-center justify-center ">
-          <PRCard1 />
-          <PRCard2 />
-          <PRCard3 />
-          <PRCard4 />
-          <PRCard5 />
+          <PRCard />
         </div>
       </div>
     </div>
