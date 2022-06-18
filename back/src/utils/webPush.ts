23c6d0d13378f1c104821bca1ae46417f65fa2d1
SongheeNoh <s0n9h2@gmail.com>
2022-06-18 17:46:30 +0900

    [BE] modify: 웹 푸시 발송 함수는 service 계층으로 이동

--- back/src/utils/webPush.ts
+++ back/src/utils/webPush.ts
@@ -1,18 +1,17 @@
 import webpush from "web-push";
-import { HttpException } from "../utils/error-util";
-import { ISendNotificationInput } from "../interfaces/subscribeInput";
 
 // VAPID keys should only be generated only once.
 const vapidKeys = {
   publicKey: process.env.PUBLIC_KEY,
   privateKey: process.env.PRIVATE_KEY,
 };
-webpush.setVapidDetails("mailto:example@yourdomain.org", vapidKeys.publicKey, vapidKeys.privateKey);
 
-const webPush = async (deviceToken: ISendNotificationInput) => {
-  await webpush.sendNotification(deviceToken, "Test Push Notification").catch((error) => {
-    throw new HttpException(500, error);
-  });
+export default (): void => {
+  webpush.setVapidDetails("mailto:example@yourdomain.org", vapidKeys.publicKey, vapidKeys.privateKey);
 };
 
-export default webPush;
+// const webPush = async (deviceToken: ISendNotificationInput) => {
+//   await webpush.sendNotification(deviceToken, "Test Push Notification").catch((error) => {
+//     throw new HttpException(500, error);
+//   });
+// };
