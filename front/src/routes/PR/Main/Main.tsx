dddb6ad8a3d8fccf6154f0383153a127c0f3bf00
YounJoo Seog <reina.seog@gmail.com>
2022-06-18 23:42:33 +0900

    [FE] style: Main Page 폰트 사이즈 리사이징, 반응형으로 변경

--- front/src/routes/PR/Main/Main.tsx
+++ front/src/routes/PR/Main/Main.tsx
@@ -13,9 +13,9 @@ const Main = () => {
       style={{ backgroundImage: "url(" + "https://r7q6w9z6.rocketcdn.me/career/wp-content/uploads/2021/05/image_processing20210222-9274-1wg7luu.gif?w=1000&h=800" + ")" }}
     >
       <div className="hero-overlay bg-opacity-60" />
-      <div className="hero-content text-center text-neutral-content">
+      <div className="hero-content text-center text-neutral-content flex flex-col md:flex-row">
         {/* // * Steps (현재 vertical) */}
-        <ul className="steps steps-vertical">
+        <ul className="steps steps-col md:steps-vertical">
           <li className="step step-primary">Search</li>
           <li className="step">AI analysis</li>
           <li className="step">Result</li>
@@ -24,7 +24,7 @@ const Main = () => {
           {/* // * 메인 Title */}
           <h1 className="mb-5 text-5xl font-bold">Pill my rhythm</h1>
           {/* // * 메인 Description */}
-          <p className="mb-5 text-sm">
+          <p className="mb-5 text-base">
             요즘 내 상태를 입력하기만 해도
             <br />
             나에게 필요한 영양제를 추천해주는 서비스가 있다면?
@@ -38,7 +38,7 @@ const Main = () => {
             <div className="form-control">
               <div className="input-group">
                 {/* // * 메인 Search창 */}
-                <input type="text" placeholder="요즘 눈이 안 좋아요... 면역력이 떨어졌어요..." className="input input-bordered w-5/6" />
+                <input type="text" placeholder="요즘 눈이 안 좋아요... 면역력이 떨어졌어요..." className="input input-bordered w-5/6 text-zinc-700 text-lg" />
                 <button className="btn btn-primary" onClick={tempNavigate}>
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
