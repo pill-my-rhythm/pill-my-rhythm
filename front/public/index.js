6ec671e8edc30604961654f0ef7f1bad2c9b15b9
SongheeNoh <s0n9h2@gmail.com>
2022-06-20 05:42:11 +0900

    [FE] add: 참고할 수 있는 console.log 추가

--- front/public/index.js
+++ front/public/index.js
@@ -2,9 +2,15 @@ if ("serviceWorker" in navigator && "PushManager" in window) {
   console.log("Service Worker and Push is supported");
 
   window.self.addEventListener("load", async () => {
-    await navigator.serviceWorker.register("./service-worker.js").catch((error) => {
-      console.error("Service Worker Error", error);
-    });
+    await navigator.serviceWorker
+      .register("./service-worker.js", { scope: "/" })
+      .then((registration) => {
+        console.log("Service worker registered.");
+        console.log("Registered at: " + registration.scope);
+      })
+      .catch((error) => {
+        console.error("Service Worker Error", error);
+      });
   });
 } else {
   console.warn("Push messaging is not supported");
