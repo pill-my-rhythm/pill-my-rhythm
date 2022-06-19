0b3fe5b27c2c248589ce1945a017783a82919aa7
SongheeNoh <s0n9h2@gmail.com>
2022-06-20 04:17:24 +0900

    [BE] add: 구독 취소 api 경로 추가

--- back/src/routes/subscribeRouter.ts
+++ back/src/routes/subscribeRouter.ts
@@ -16,6 +16,14 @@ SubscribeRouter.post(
   SubscribeController.subscribeNotification,
 );
 
+// 알림 서비스 구독
+SubscribeRouter.post(
+  "/delete",
+  // [check("device_token").exists().isJSON(), validatorErrorChecker],
+  [check("device_token").exists(), validatorErrorChecker],
+  SubscribeController.subscribeNotification,
+);
+
 // 사용자별 푸시 알림 테스트
 SubscribeRouter.get("/push-test", SubscribeController.pushNotification);
 
