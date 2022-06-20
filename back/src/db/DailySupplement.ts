1dc74a9bb98d7c2ef8b347e109e66c0526066bed
SongheeNoh <s0n9h2@gmail.com>
2022-06-20 19:26:02 +0900

    [BE] add: 하루 영양제 푸시 알림 정보에 필요한 DailySupplement 정보 가져와서 전처리 중~

--- back/src/db/DailySupplement.ts
+++ back/src/db/DailySupplement.ts
@@ -14,6 +14,20 @@ const DailySupplement = {
     });
     return schedule;
   },
+  findByIdAndToDo: async (schedule: any) => {
+    const fk_user_id = schedule.fk_user_id;
+    const to_do = schedule.to_do;
+    const dailySchedule = await DailySupplements.findAll({
+      attributes: ["fk_supplement_id"],
+      where: { type: to_do },
+      include: [
+        { model: Users, attributes: [], where: { pk_user_id: fk_user_id } },
+        { model: Supplements, attributes: ["name"] },
+      ],
+    });
+    const pushData = dailySchedule.map((element) => element.get({ plain: true }));
+    return pushData;
+  },
   createDailySchedule: async (data: IDailySupplementCreateInput) => {
     const dailySchedule = await DailySupplements.create(data);
     return dailySchedule;
