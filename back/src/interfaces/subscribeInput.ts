28e9b109c99c895244e898432009c1e74216bd6d
SongheeNoh <s0n9h2@gmail.com>
2022-06-20 20:32:23 +0900

    [BE] add: pushData interface 정의

--- back/src/interfaces/subscribeInput.ts
+++ back/src/interfaces/subscribeInput.ts
@@ -3,3 +3,9 @@ export interface ISendNotificationInput {
   expirationTime?: any;
   keys: { p256dh: string; auth: string };
 }
+
+export interface pushData {
+  user_name: string;
+  to_do: string;
+  supplements: string;
+}
