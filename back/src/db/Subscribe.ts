3aa97f92664d7f969ec6fff540d82098adeaa6b1
SongheeNoh <s0n9h2@gmail.com>
2022-06-22 01:26:58 +0900

    [BE] add: findByUserAndDevice 외래키로 조회하도록 변경

--- back/src/db/Subscribe.ts
+++ back/src/db/Subscribe.ts
@@ -1,9 +1,13 @@
 import { Subscribes } from "./models/subscribe";
 import { ISendNotificationInput } from "../interfaces/subscribeInput";
+import { Users } from "./models/user";
 
 const Subscribe = {
   findByUserAndDevice: async (fk_user_id: string, device_token: ISendNotificationInput) => {
-    const subscription = await Subscribes.findOne({ where: { fk_user_id, device_token } });
+    const subscription = await Subscribes.findOne({
+      where: { device_token },
+      include: { model: Users, attributes: [], where: { pk_user_id: fk_user_id } },
+    });
     return subscription;
   },
 
