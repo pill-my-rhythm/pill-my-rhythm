449d2d078a426290ed90d7f577a465f23c506c1a
SongheeNoh <s0n9h2@gmail.com>
2022-06-22 04:00:01 +0900

    [FE] add: 푸시 알림 전송 옵션에 actions 추가

--- front/public/service-worker.js
+++ front/public/service-worker.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-restricted-globals */
+// Service Workers 설정에서 Update on reload는 이 파일이 변경됐을 때만 체크
 
 // 서비스 워커에서 발생하는(back에서 보낸) 푸시 이벤트를 수신
 // self는 서비스 워커 자체를 참조
@@ -14,13 +15,39 @@ self.addEventListener("push", (event) => {
       // sound: "./"
 
       body: data.body,
+      actions: [
+        {
+          action: "homepage-action",
+          title: "Pill my rhythm",
+          // icon: "/images/demos/action-1-128x128.png",
+        },
+        {
+          action: "checklist-action",
+          title: "Checklist",
+          // icon: "/images/demos/action-4-128x128.png",
+        },
+      ],
     }),
   );
 });
 
-self.addEventListener("notificationclick", (event) => {
-  // 알림창 닫음
-  event.notification.close();
-  // URL을 로드하는 새 창이나 탭이 열림
-  event.waitUntil(self.clients.openWindow("http://localhost:3000"));
-});
+self.addEventListener(
+  "notificationclick",
+  (event) => {
+    // 알림창 닫음
+    event.notification.close();
+    // URL을 로드하는 새 창이나 탭이 열림
+    switch (event.action) {
+      case "homepage-action":
+        event.waitUntil(self.clients.openWindow("http://localhost:3000"));
+        break;
+      // User selected the Archive action.
+      case "checklist-action":
+        // TODO: front 구현 후 링크 수정해야 함
+        event.waitUntil(self.clients.openWindow("http://localhost:3000/schedule/checklist"}`));
+        break;
+      // no default
+    }
+  },
+  false,
+);
