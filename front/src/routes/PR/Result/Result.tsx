6b8da3d9e3dca2f4876c3f872ae6ab010d84f1a4
YounJoo Seog <reina.seog@gmail.com>
2022-06-21 15:45:33 +0900

    [FE] Add: User 값 전달은 되는데... 오류 존재

--- front/src/routes/PR/Result/Result.tsx
+++ front/src/routes/PR/Result/Result.tsx
@@ -5,16 +5,7 @@ import Promotion from "./Promotion";
 import { UserStateContext } from "../../../Dispatcher";
 
 export interface UserData {
-  age_range: string;
-  createdAt: Date;
-  deletedAt?: null;
-  email: string;
-  gender: string;
-  job: string;
-  password: string;
-  pk_user_id: string;
-  updatedAt: Date;
-  user_name: string;
+  user: { age_range: string; createdAt: Date; deletedAt: null; email: string; gender: string; job: string; password: string; pk_user_id: string; updatedAt: Date; user_name: string };
 }
 
 const Result = () => {
@@ -31,8 +22,7 @@ const Result = () => {
   return (
     <div className="overflow-scroll:hidden">
       <VideoArea />
-      {/* //! 현재 user={user}로 props 전달 안 됌,, 수정할 것 => '{ user: UserData | undefined; }' 형식은 'IntrinsicAttributes & UserData' 형식에 할당할 수 없습니다. 'IntrinsicAttributes & UserData' 형식에 'user' 속성이 없습니다. */}
-      <RecommendationArea />
+      <RecommendationArea user={user} isLogin={isLogin} />
       <Promotion />
     </div>
   );
