5fb14c704c86e217598e42fb871159a071f2784b
SongheeNoh <s0n9h2@gmail.com>
2022-06-22 01:59:03 +0900

    [BE] add: findByUserId 메소드 수정

--- back/src/db/Subscribe.ts
+++ back/src/db/Subscribe.ts
@@ -17,8 +17,9 @@ const Subscribe = {
   },
 
   findByUserId: async (fk_user_id: string) => {
-    // const devicesArray = await Subscribes.findAll({ where: { fk_user_id } });
-    const devicesArray = await Subscribes.findAll();
+    const devicesArray = await Subscribes.findAll({
+      include: { model: Users, attributes: [], where: { pk_user_id: fk_user_id } },
+    });
     return devicesArray;
   },
 
