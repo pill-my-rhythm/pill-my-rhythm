6cd4084b2077f161631099947dd0963f409fed32
SongheeNoh <s0n9h2@gmail.com>
2022-06-22 01:36:34 +0900

    [BE] add: ISubscriptionInput 추가

--- back/src/interfaces/subscribeInput.ts
+++ back/src/interfaces/subscribeInput.ts
@@ -1,3 +1,8 @@
+export interface ISubscriptionInput {
+  fk_user_id: string;
+  device_token: ISendNotificationInput;
+}
+
 export interface ISendNotificationInput {
   endpoint: string;
   expirationTime?: any;
