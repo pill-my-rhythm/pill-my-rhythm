7fdfba384c446e32e9e564f885ec7877af6e52fb
SongheeNoh <s0n9h2@gmail.com>
2022-06-22 03:47:05 +0900

    [FE] add: webpush 전송 옵션에 icon, badge, vibrate 추가

--- front/public/service-worker.js
+++ front/public/service-worker.js
@@ -3,23 +3,24 @@
 // 서비스 워커에서 발생하는(back에서 보낸) 푸시 이벤트를 수신
 // self는 서비스 워커 자체를 참조
 self.addEventListener("push", (event) => {
-  console.log("[Service Worker] Push Received.");
-  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
-
   const data = event.data.json();
-  // 브라우저는 전달된 Promise가 확인될 때까지 서비스 워커를 활성화 및 실행 상태로 유지
-  self.registration.showNotification(data.title, {
-    body: data.body,
-  });
 
   // 브라우저는 전달된 Promise가 확인될 때까지 서비스 워커를 활성화 및 실행 상태로 유지
-  // event.waitUntil(window.self.registration.showNotification("Pill my rhythm", options));
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      icon: "./icon-192x192.png",
+      badge: "./badge-72x72.png", // android에서만 보임
+      vibrate: [200, 100, 200, 100, 200, 100, 200], // android에서만 동작
+      // sound: "./"
+
+      body: data.body,
+    }),
+  );
 });
 
-// window.self.addEventListener("notificationclick", (event) => {
-//   console.log("[Service Worker] Notification click Received.");
-//   // 알림창 닫음
-//   event.notification.close();
-//   // URL을 로드하는 새 창이나 탭이 열림
-//   event.waitUntil(window.self.clients.openWindow("https://developers.google.com/web/"));
-// });
+self.addEventListener("notificationclick", (event) => {
+  // 알림창 닫음
+  event.notification.close();
+  // URL을 로드하는 새 창이나 탭이 열림
+  event.waitUntil(self.clients.openWindow("http://localhost:3000"));
+});
