20f1327298d3a03ca60cceebf80e4aeab9544247
YounJoo Seog <reina.seog@gmail.com>
2022-06-22 15:34:53 +0900

    [FE] Modify: Props 전달 방식 변경중..

--- front/src/routes/PR/Result/Result.tsx
+++ front/src/routes/PR/Result/Result.tsx
@@ -1,29 +1,21 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import VideoArea from "./VideoArea";
 import RecommendationArea from "./RecommendationArea";
 import Promotion from "./Promotion";
 import { UserStateContext } from "../../../Dispatcher";
 
 export interface Props {
-  user: { age_range: string; createdAt: Date; deletedAt?: null; email: string; gender: string; job: string; password: string; pk_user_id: string; updatedAt: Date; user_name: string };
   isLogin: Boolean;
 }
 
 const Result = () => {
   const userState = useContext(UserStateContext);
   const isLogin = !!userState.user;
-  const [user, setUser] = useState<Props["user"]>();
-
-  useEffect(() => {
-    setUser(userState.user);
-  }, []);
-
-  console.log("#ResultPg_user", user);
 
   return (
     <div className="overflow-scroll:hidden">
       <VideoArea />
-      <RecommendationArea user={user!} isLogin={isLogin} />
+      <RecommendationArea isLogin={isLogin} />
       <Promotion />
     </div>
   );
