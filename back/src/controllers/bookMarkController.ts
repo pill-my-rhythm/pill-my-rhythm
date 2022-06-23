21d38fb785759b9030e75286e6684200708efd46
parkjungyoon <tbr06057@naver.com>
2022-06-23 13:04:19 +0900

    [BE] refact: 상태코드 변경

--- back/src/controllers/bookMarkController.ts
+++ back/src/controllers/bookMarkController.ts
@@ -6,7 +6,7 @@ const BookMarkController = {
     try {
       const fk_user_id: string = req.currentUserId;
       const bookmarks = await BookMarkService.getAllBookmarks(fk_user_id);
-      res.status(201).json(bookmarks);
+      res.status(200).json(bookmarks);
     } catch (error) {
       next(error);
     }
@@ -18,7 +18,7 @@ const BookMarkController = {
       const fk_supplement_id = Number(req.params.supplement_id);
       const newBookMark = await BookMarkService.addBookMark({ fk_user_id, fk_supplement_id });
 
-      res.status(201).json(newBookMark);
+      res.status(200).json(newBookMark);
     } catch (error) {
       next(error);
     }
@@ -30,7 +30,7 @@ const BookMarkController = {
       const pk_bookmark_id = Number(req.params.bookmark_id);
       const deletedBookmark = await BookMarkService.deleteBookmark(fk_user_id, pk_bookmark_id);
 
-      res.status(201).json(deletedBookmark);
+      res.status(200).json(deletedBookmark);
     } catch (error) {
       next(error);
     }
