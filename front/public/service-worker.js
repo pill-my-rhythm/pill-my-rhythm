7498107473ca45bda3b987032de4f871886aca2c
SongheeNoh <s0n9h2@gmail.com>
2022-06-23 20:39:56 +0900

    [FE] add: 아직 수정중, web-push에 토큰 정보 넣는 것 구현중

--- front/public/service-worker.js
+++ front/public/service-worker.js
@@ -24,15 +24,18 @@ self.addEventListener("push", (event) => {
         {
           action: "checklist-action",
           title: "Checklist",
+          data: {
+            jwt: data.jwtToken,
+          },
           // icon: "/images/demos/action-4-128x128.png",
         },
       ],
       // data로 action 실행 시 객체 전송 가능
       // TODO: 이건 BE로 보내서 checklist 작성 위해 토큰값 전송해야 할 듯, 근데 토큰이 refresh되는건 이제.. 생각해보자
-      data: {
-        time: new Date(Date.now()).toString(),
-        message: "Hello, World!",
-      },
+      // data: {
+      //   time: new Date(Date.now()).toString(),
+      //   message: "Hello, World!",
+      // },
 
       requireInteraction: true, // chrome과 같이 충분히 큰 창에서 사용자가 직접 닫을 때까지 알림 사라지지 않음
     }),
@@ -47,18 +50,14 @@ self.addEventListener(
     // URL을 로드하는 새 창이나 탭이 열림
     switch (event.action) {
       case "homepage-action":
-        const notificationData = event.notification.data;
-        console.log("The data notification had the following parameters:");
-        Object.keys(notificationData).forEach((key) => {
-          console.log(`  ${key}: ${notificationData[key]}`);
-        });
-        console.log("");
-        event.waitUntil(self.clients.openWindow("http://localhost:3000"));
+        event.waitUntil(self.clients.openWindow(`${process.env.REACT_APP_MODE}:${process.env.REACT_APP_FRONT_PORT}`));
         break;
       // User selected the Archive action.
       case "checklist-action":
-        // TODO: front 구현 후 링크 수정해야 함
-        event.waitUntil(self.clients.openWindow("http://localhost:3000/schedule/checklist"));
+        console.log(event);
+        const jwtToken = event.notification.data;
+
+        event.waitUntil(self.clients.openWindow(`${process.env.REACT_APP_MODE}:${process.env.REACT_APP_FRONT_PORT}/m/checklist?jwt=${jwtToken}`));
         break;
       // no default
     }
