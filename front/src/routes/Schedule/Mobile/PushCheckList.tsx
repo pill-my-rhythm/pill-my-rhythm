75d5daec4214823e964d01b16c52311489b4bea2
SongheeNoh <s0n9h2@gmail.com>
2022-06-24 15:12:30 +0900

    [FE] feat: 오늘의 체크리스트 모바일 겸용 페이지 생성

--- front/src/routes/Schedule/Mobile/PushCheckList.tsx
+++ front/src/routes/Schedule/Mobile/PushCheckList.tsx
@@ -5,65 +5,93 @@
 // webpush data에 jwt_token값 들어 있음
 // 해당 jwt_token이랑 체크리스트 선택값으로 체크리스트 생성하도록 backend에 요청
 
-// import React from "react";
-// import axios from "axios";
-// import styled from "styled-components";
+import React, { useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import styled from "styled-components";
+import axios from "axios";
+import { tasksAtom } from "../../../atoms";
 
-// const TodoWrapper = styled.div`
-//   display: flex;
-//   text-align: left;
-//   color: black;
-//   padding: 10px;
-// `;
-
-// interface CheckListProp {
-//   data: { text: string };
-//   index: number;
-//   tasks: { text: string }[];
-// }
+const TodoWrapper = styled.div`
+  display: flex;
+  text-align: left;
+  color: black;
+  padding: 10px;
+`;
 
 const Checklist = () => {
-  //   const queryString = new URLSearchParams(window.location.search);
-  //   let jwtToken = queryString.get("jwt");
-  //   console.log(jwtToken);
-  //   const submit = async () => {
-  //     console.log("checklist submit function");
-  //     // 체크리스트 항목 정보 DB에 추가
-  //     await axios
-  //       .post(
-  //         `${process.env.REACT_APP_MODE}:${process.env.REACT_APP_BACK_PORT}/checklist/create`,
-  //         { device_token: subscription },
-  //         {
-  //           headers: {
-  //             "Content-Type": "application/json",
-  //             Authorization: `Bearer ${jwtToken}`,
-  //           },
-  //         },
-  //       )
-  //       .then(() => {
-  //         alert("오늘의 체크리스트 작성이 완료되었습니다.");
-  //       });
-  //   };
+  const queryString = new URLSearchParams(window.location.search);
+  let jwtToken = queryString.get("jwt");
+
+  const tasks = useRecoilValue(tasksAtom);
+  const [checkedItems, setCheckedItems]: any = useState([]);
+
+  const checkedItemHandler = (checked: boolean, id: string) => {
+    if (checked) {
+      setCheckedItems([...checkedItems, id]);
+    } else {
+      // 체크 해제
+      setCheckedItems(checkedItems.filter((el: any) => el !== id));
+    }
+  };
+
+  let today: string;
+  const now = new Date();
+  const month = `${now.getUTCMonth() + 1}`.length === 1 ? "0" + `${now.getUTCMonth() + 1}` : now.getUTCMonth() + 1; //months from 1-12
+  const day = now.getUTCDate();
+  const year = now.getUTCFullYear();
+  today = year + "-" + month + "-" + day;
+
+  const submit = async () => {
+    console.log("checklist submit function");
+    console.log(tasks);
+    const result = tasks.map((el: any) => (checkedItems.includes(el.text) ? true : false));
+    console.log(result);
+
+    const checklistData = { date: today, one: result[0], two: result[1], three: result[2], four: result[3], five: result[4], six: result[5] };
+    console.log(checklistData);
+
+    // 체크리스트 항목 정보 DB에 추가
+    await axios
+      .post(`${process.env.REACT_APP_MODE}:${process.env.REACT_APP_BACK_PORT}/checklist/create`, checklistData, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${jwtToken}`,
+        },
+      })
+      .then(() => {
+        alert("오늘의 체크리스트 작성이 완료되었습니다.");
+      });
+  };
+
   return (
-    <>
-      {/* <DateLabel htmlFor={`modal-${data.text}`} className="modal-button cursor-pointer">
-          {data.text}
-        </DateLabel> */}
-      <input type="checkbox" id={`modal-${data.text}`} className="modal-toggle" />
-      <label htmlFor={`modal-${data.text}`} className="modal cursor-pointer">
-        <label className="modal-box max-w-xs" htmlFor="">
+    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Today's Checklist</h2>
+          <br />
+          <p className="mt-2 text-center text-sm text-gray-600">{today}</p>
+        </div>
+        <div>
+          {/* <label className="modal-box max-w-xs" htmlFor=""> */}
           {tasks.map((task) => (
             <TodoWrapper key={task.text}>
-              <input type="checkbox" className="checkbox checkbox-sm checkbox-primary mr-3" />
+              <input
+                type="checkbox"
+                className="checkbox checkbox-sm checkbox-primary mr-3"
+                onChange={(e) => {
+                  checkedItemHandler(e.currentTarget.checked, task.text);
+                }}
+                checked={checkedItems.includes(task.text) ? true : false}
+              />
               {task.text}
             </TodoWrapper>
           ))}
-        </label>
-        <button className="btn btn-primary" onClick={() => submit()}>
-          제출
-        </button>
-      </label>
-    </>
+          <button className="btn btn-primary" onClick={() => submit()}>
+            제출
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
