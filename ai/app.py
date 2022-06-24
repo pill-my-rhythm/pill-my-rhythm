cc63627066a404e5ba5f2f9010237b2aee404454
parkjungyoon <tbr06057@naver.com>
2022-06-24 10:32:30 +0900

    [BE] add: verifyToken 로직 추가

--- ai/app.py
+++ ai/app.py
@@ -1,7 +1,7 @@
 import pymysql
 from test_model import test_model
-from flask import Flask, request, jsonify
-
+from flask import Flask, request, g
+from verifyToken import verifyToken
 import os 
 from dotenv import load_dotenv
 load_dotenv()
@@ -23,17 +23,28 @@ cursor = db.cursor()
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
 @app.route("/recommend", methods=['POST'])
+@verifyToken
 def recommend():
     sentence = request.get_json()["sentence"]
+    fk_user_id = g.userId
     supplement = test_model(sentence)
+
     recommend_supplement = []
     for i in supplement:
-        sql = """SELECT * FROM tb_supplement WHERE pk_supplement_id = '%s'""" % (i+1)
-        cursor.execute(sql)
-        print(cursor.description[0])
+        select_sql = """SELECT * FROM tb_supplement WHERE pk_supplement_id = '%s'""" % (i+1)
+        cursor.execute(select_sql)
         recommend_supplement.append(dict(zip([column[0] for column in cursor.description], cursor.fetchone())))
-    db.close()
+    
+    if fk_user_id is not None: 
+        insert_sql = """INSERT INTO tb_recommend (created_at, updated_at, fk_user_id, fk_supplement_id) VALUES (now(), now(), fk_user_id, %s)"""
+        for idx in supplement: 
+            print(idx)
+            cursor.execute(insert_sql, (idx))
+        db.commit()
+    
+    # db.close()
     return {'results': recommend_supplement}
 
 
