f2c59b62ead2d988dd96ba9e8dfd95683deffcf6
parkjungyoon <tbr06057@naver.com>
2022-06-25 12:53:43 +0900

    [BE] refact: error status 반환

--- back/src/middlewares/errorMiddleware.ts
+++ back/src/middlewares/errorMiddleware.ts
@@ -4,7 +4,7 @@ import { HttpException } from "../utils/error-util";
 function errorMiddleware(error: HttpException, req: Request, res: Response, next: NextFunction) {
   console.log("\x1b[33m%s\x1b[0m", error);
   const status = error.status || 500;
-  res.json({
+  res.status(status).json({
     status: status,
     message: error.message,
   });
