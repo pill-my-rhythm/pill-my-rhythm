ce27e0433d555dea474b47972a6e9aaaf0dbb896
SongheeNoh <s0n9h2@gmail.com>
2022-06-27 20:10:18 +0900

    [BE] feat: winston으로 로그 파일 생성

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import morgan from "morgan";
+import { stream } from "./utils/winston";
 
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
@@ -16,14 +17,14 @@ import { errorMiddleware } from "./middlewares/errorMiddleware";
 const app: express.Application = express();
 const swaggerSpec = YAML.load(path.join(__dirname, "./swagger.yaml"));
 
-// morgan (request, response formatting)
-app.use(morgan("combined"));
 // swagger
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// morgan (request, response formatting)
+app.use(morgan("combined", { stream }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hello typescript express!");
