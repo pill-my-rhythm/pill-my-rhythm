2aaa5e44fe7835ac96c4ef901b094bf51c0821f4
YounJoo Seog <reina.seog@gmail.com>
2022-06-28 02:24:14 +0900

    [FE] feat : Scrolldown Button 추가

--- front/src/routes/PR/Result/VideoArea.tsx
+++ front/src/routes/PR/Result/VideoArea.tsx
@@ -3,28 +3,44 @@ import React from "react";
 const VideoArea = () => {
   return (
     <div className="min-h-screen bg-gradient-to-tr from-[#7FDCDC] to-[#E3F2ED]">
-      <div className="w-screen min-h-screen p-2 flex flex-wrap flex-row-reverse items-center justify-center">
-        <div className="p-4 w-full md:w-5/12 ">
-          <iframe className="w-full aspect-video rounded-lg shadow-2xl" src="https://www.youtube.com/embed/LTdLVar1FUU" title="youtube" />
+      <div className="min-h-screen flex flex-col items-center justify-center">
+        <div className="w-screen p-2 flex flex-wrap flex-row-reverse items-center justify-center">
+          <div className="p-4 w-full md:w-5/12 ">
+            <iframe className="w-full aspect-video rounded-lg shadow-2xl" src="https://www.youtube.com/embed/LTdLVar1FUU" title="youtube" />
+          </div>
+          <div className="p-2 m-2 break-words text-center">
+            <h1 className="text-5xl font-bold text-white mr-6 ml-6 leading-normal">
+              영양제, 복용하기 전에
+              <br />
+              정확하게 알고 먹자!
+            </h1>
+            <p className="m-6 leading-6">
+              당신에게 가장 필요한 영양제는 “something” 이예요. <br />
+              “something”을 섭취하기 전에 주의할 점을 영상을 통해 확인해보세요😃
+            </p>
+          </div>
+          <ul className="steps md:steps-vertical mb-8">
+            <li className="step step-accent">Search</li>
+            <li className="step step-accent">AI analysis</li>
+            <li className="step step-accent" data-content="✓">
+              Result
+            </li>
+          </ul>
         </div>
-        <div className="p-2 m-2 break-words text-center">
-          <h1 className="text-5xl font-bold text-white mr-6 ml-6 leading-normal">
-            영양제, 복용하기 전에
-            <br />
-            정확하게 알고 먹자!
-          </h1>
-          <p className="m-6 leading-6">
-            당신에게 가장 필요한 영양제는 “something” 이예요. <br />
-            “something”을 섭취하기 전에 주의할 점을 영상을 통해 확인해보세요😃
-          </p>
+        <div className="md:h-80 flex items-end">
+          <button
+            className="bg-transparent flex scroll-smooth"
+            onClick={() =>
+              window.scrollBy({
+                top: window.innerHeight,
+                left: 0,
+                behavior: "smooth",
+              })
+            }
+          >
+            <img src="https://blog.kakaocdn.net/dn/8QI8z/btrBuyNOXqU/iktp61W5CxsECbB0qVKQz0/img.png" alt="scrolldown" width={120} />
+          </button>
         </div>
-        <ul className="steps md:steps-vertical mb-8">
-          <li className="step step-accent">Search</li>
-          <li className="step step-accent">AI analysis</li>
-          <li className="step step-accent" data-content="✓">
-            Result
-          </li>
-        </ul>
       </div>
     </div>
   );
