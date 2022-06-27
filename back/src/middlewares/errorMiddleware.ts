cb6cf26802eb80ab28eebf130a81e1b95782a3d0
SongheeNoh <s0n9h2@gmail.com>
2022-06-28 00:51:00 +0900

    [BE] add: 배포 시에만 로그 찍히도록 변경

--- back/src/middlewares/errorMiddleware.ts
+++ back/src/middlewares/errorMiddleware.ts
@@ -1,14 +1,17 @@
 import { Request, Response, NextFunction } from "express";
 import moment from "moment";
+import "moment-timezone";
 import { logger } from "../utils/winston";
 import { HttpException } from "../utils/error-util";
+
+moment.tz.setDefault("Asia/Seoul"); // 로그 시간대 한국 기준으로 변경
 
 const errorMiddleware = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
   console.log("\x1b[33m%s\x1b[0m", error);
   const status = error.status || 500;
 
   // 배포시에만 log 남김
-  if (process.env.APP_MODE === "production") {
+  if (process.env.NODE_ENV === "production") {
     const errObj = {
       req: {
         headers: req.headers,
