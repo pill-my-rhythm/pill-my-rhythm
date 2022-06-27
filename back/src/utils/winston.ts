d573b09c6c75d81a1dfcedd525d665d75fdfff83
SongheeNoh <s0n9h2@gmail.com>
2022-06-28 00:26:58 +0900

    [BE] add: 30일 동안만 저장되도록 변경

--- back/src/utils/winston.ts
+++ back/src/utils/winston.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import winston from "winston";
+import "winston-daily-rotate-file";
 
 const logDir = __dirname + "/../logs";
 
@@ -7,16 +8,18 @@ if (!fs.existsSync(logDir)) {
   fs.mkdirSync(logDir);
 }
 
-const infoTransport = new winston.transports.File({
+const infoTransport = new winston.transports.DailyRotateFile({
   filename: "info.log",
   dirname: logDir,
   level: "info",
+  maxFiles: "30d", // 30일치 저장
 });
 
-const errorTransport = new winston.transports.File({
+const errorTransport = new winston.transports.DailyRotateFile({
   filename: "error.log",
   dirname: logDir,
   level: "error",
+  maxFiles: "30d", // 30일치 저장
 });
 
 const logger = winston.createLogger({
