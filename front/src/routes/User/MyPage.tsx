93532858b19cc2178021f05bc58c07d4678828f2
YounJoo Seog <reina.seog@gmail.com>
2022-06-28 22:49:31 +0900

    [FE] refact : 안 쓰는 코드 삭제

--- front/src/routes/User/MyPage.tsx
+++ front/src/routes/User/MyPage.tsx
@@ -9,7 +9,6 @@ const MyPage = () => {
 
   return (
     <>
-      {/* <img className="w-screen" src="https://blog.kakaocdn.net/dn/lYHux/btrE4nVMvMo/Hn1wvs9Mgf7m7m2oo4boB0/img.png" alt="목업 데이터" /> */}
       <UserMyPage />
       <UserBookMarkList />
       <UserRecommendPage />
