cc4edd73da421c6409fe13981e748f9e678c1137
YounJoo Seog <reina.seog@gmail.com>
2022-06-28 23:46:20 +0900

    [FE] style : 모바일 페이지 대응 추가, 반응형 메뉴바 변경

--- front/src/routes/_shared/Header/Header.tsx
+++ front/src/routes/_shared/Header/Header.tsx
@@ -1,16 +1,27 @@
-import React, { useContext } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate, Link, useLocation } from "react-router-dom";
 import { UserStateContext, DispatchContext } from "../../../Dispatcher";
 import { del } from "../../../Api";
 
 const Header = () => {
   const navigate = useNavigate();
-
+  const location = useLocation();
   const userState = useContext(UserStateContext);
   const dispatch = useContext(DispatchContext);
 
+  const [mobile, setMobile] = useState<Boolean>(false);
+
   // 전역상태에서 user가 null이 아니라면 로그인 성공 상태임.
   const isLogin = !!userState.user;
+
+  // 모바일 체크 함수
+  const checkParams = () => {
+    const currentParams = location.pathname;
+    // console.log("params", currentParams);
+    if (currentParams === "/m/subscribe" || "/m/checklist") {
+      setMobile(true);
+    } else setMobile(false);
+  };
 
   // 로그아웃 클릭 시 실행되는 함수
   const logout: React.MouseEventHandler<HTMLButtonElement> = async () => {
@@ -46,15 +57,35 @@ const Header = () => {
     }
   };
 
-  return (
-    <div className="navbar flex-col md:flex-row w-full bg-base-100 sticky top-0 z-40 shadow-md">
-      <div className="flex-1">
-        <Link to="/" className="btn btn-ghost normal-case text-xl">
+  useEffect(() => {
+    checkParams();
+  }, [location]);
+
+  return !mobile ? (
+    <div className="navbar flex-col md:flex-row w-full bg-base-100 sticky top-0 z-40 shadow-md justify-center items-center">
+      <div className="flex">
+        <Link to="/" className="btn btn-ghost normal-case text-xl flex">
           <img src="https://blog.kakaocdn.net/dn/bro2IW/btrEji2iHDE/gJHWwqC1zfOCxRpv2cOwP0/img.png" alt="icon" width={30} height={30} className="mr-2" />
           Pill my rhythm
         </Link>
       </div>
-      <div className="flex-none">
+    </div>
+  ) : (
+    <div className="navbar flex-row w-full bg-base-100 sticky top-0 z-40 shadow-md">
+      <div className="flex-1 justify-between">
+        <Link to="/" className="btn btn-ghost normal-case text-xl">
+          <img src="https://blog.kakaocdn.net/dn/bro2IW/btrEji2iHDE/gJHWwqC1zfOCxRpv2cOwP0/img.png" alt="icon" width={30} height={30} className="mr-2" />
+          Pill my rhythm
+        </Link>
+        <div className="md:hidden">
+          <button className="btn btn-square btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div className="bg-red-200 md:flex hidden">
         <ul className="menu menu-horizontal p-0 text-center">
           <li>
             <Link to="/">Search</Link>
@@ -66,28 +97,39 @@ const Header = () => {
           <li>
             <Link to="/">Contact us</Link>
           </li>
-          <li tabIndex={0}>
-            {!isLogin ? (
+          {!isLogin ? (
+            <li tabIndex={0}>
               <Link to="/login">
                 Login
                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                   <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
                 </svg>
               </Link>
-            ) : (
-              <button onClick={logout}>
-                Logout
+              <ul className="p-2 bg-base-100">
+                <li>
+                  <button onClick={ControlMyPage}>My Page</button>
+                </li>
+              </ul>
+            </li>
+          ) : (
+            <li tabIndex={0}>
+              <button onClick={ControlMyPage}>
+                My Page
                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                   <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
                 </svg>
               </button>
-            )}
-            <ul className="p-2 bg-base-100">
-              <li>
-                <button onClick={ControlMyPage}>My Page</button>
-              </li>
-            </ul>
-          </li>
+              <ul className="p-2 bg-base-100">
+                <li>
+                  <button className="px-8" onClick={logout}>
+                    Logout
+                  </button>
+                </li>
+              </ul>
+            </li>
+          )}
+          {/* <ul className="p-2 bg-base-100">
+           */}
         </ul>
       </div>
     </div>
