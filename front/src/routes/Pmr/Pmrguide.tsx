656557f26616095a40e1b30972d6f140bc7cb4a0
YounJoo Seog <reina.seog@gmail.com>
2022-06-30 01:51:59 +0900

    [FE] Add : pnr 페이지 형태 추가

--- front/src/routes/Pmr/Pmrguide.tsx
+++ front/src/routes/Pmr/Pmrguide.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Pmrguide = () => {
-  return <div>hello</div>;
+  return <div className="w-screen min-h-screen bg-gradient-to-tr from-[#7FDCDC] to-[#E3F2ED]">hello</div>;
 };
 
 export default Pmrguide;
