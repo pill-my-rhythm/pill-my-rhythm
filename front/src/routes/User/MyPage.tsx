015dd43e2a2f838ce12b88502e2e4b2ea5c34748
YounJoo Seog <reina.seog@gmail.com>
2022-07-01 21:27:32 +0900

    [FE] feat : 마이페이지에 go to top 버튼 추가

--- front/src/routes/User/MyPage.tsx
+++ front/src/routes/User/MyPage.tsx
@@ -19,11 +19,27 @@ const MyPage = () => {
 
   return (
     <>
-      {/* <UserMyPage Recoiluser={Recoiluser} /> */}
+      <UserMyPage Recoiluser={Recoiluser} />
       <MyYearlyChecklist />
       <UserBookMarkList />
-      {/* <UserRecommendPage Recoiluser={Recoiluser} /> */}
+      <UserRecommendPage Recoiluser={Recoiluser} />
+      <div className="bg-gradient-to-tr from-[#7FDCDC] to-[#E3F2ED] flex justify-center py-12 px-4 sm:px-6 lg:px-8 flex items-center">
+        <button
+          className="bg-transparent flex scroll-smooth"
+          onClick={() =>
+            window.scrollTo({
+              top: 0,
+              left: 0,
+              behavior: "smooth",
+            })
+          }
+        >
+          <img src="https://blog.kakaocdn.net/dn/RL8Kv/btrBr5TDbYj/dklV6QQr0hgYlTWfr1AVbk/img.png" alt="scrolltop" width={120} />
+        </button>
+      </div>
     </>
   );
 };
 export default MyPage;
+
+// https://blog.kakaocdn.net/dn/RL8Kv/btrBr5TDbYj/dklV6QQr0hgYlTWfr1AVbk/img.png
