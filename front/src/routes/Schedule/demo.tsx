9c5f415fa46bb61ad911993dd0c37ee2b07a2847
SongheeNoh <s0n9h2@gmail.com>
2022-07-01 14:54:43 +0900

    [FE] modify: 필요 없는 코드 삭제

--- front/src/routes/Schedule/demo.tsx
+++ front/src/routes/Schedule/demo.tsx
@@ -3,18 +3,6 @@ import Tooltip from "@uiw/react-tooltip";
 import HeatMap from "@uiw/react-heat-map";
 import { get } from "../../Api";
 
-const value = [
-  { date: "2022/01/11", count: 2 },
-  ...[...Array(17)].map((_, idx) => ({ date: `2022/01/${idx + 10}`, count: idx })),
-  ...[...Array(17)].map((_, idx) => ({ date: `2022/02/${idx + 10}`, count: idx })),
-  { date: "2022/04/12", count: 2 },
-  { date: "2022/05/01", count: 5 },
-  { date: "2022/05/02", count: 5 },
-  { date: "2022/05/03", count: 1 },
-  { date: "2022/05/04", count: 11 },
-  { date: "2022/05/08", count: 32 },
-];
-
 const Demo = () => {
   const [data, setData] = useState();
   const LoadYearlyData = async () => {
