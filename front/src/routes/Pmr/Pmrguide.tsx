ac8b01caf66d395ee8b569b508111b9c34c55884
YounJoo Seog <reina.seog@gmail.com>
2022-07-01 15:00:35 +0900

    [FE] feat : about pmr 페이지 완성(스케쥴러 제외)

--- front/src/routes/Pmr/Pmrguide.tsx
+++ front/src/routes/Pmr/Pmrguide.tsx
@@ -1,7 +1,69 @@
 import React from "react";
 
 const Pmrguide = () => {
-  return <div className="w-screen min-h-screen bg-gradient-to-tr from-[#7FDCDC] to-[#E3F2ED]">hello</div>;
+  return (
+    <div className="w-screen min-h-screen bg-gradient-to-tl from-[#7FDCDC] to-[#E3F2ED] flex items-center justify-center">
+      {" "}
+      <div className="w-full md:w-[68.75rem] py-20 min-h-screen flex flex-col items-center justify-center">
+        <div className="w-fit p-2 flex flex-wrap items-center justify-center">
+          <div className="p-2 w-full md:w-5/12 ">
+            <img src="https://blog.kakaocdn.net/dn/wDgQv/btrGet16eNM/UKyMuTzHQpS7QevKRDgp3K/img.gif" alt="search service" className="rounded-lg shadow-2xl w-[650px]" />
+          </div>
+          <div className="p-2 m-2 break-words text-center">
+            <h1 className="text-4xl md:text-5xl font-bold leading-relaxed md:leading-normal text-white mx-3 md:mx-6">
+              AI 문장 분석
+              <br />
+              영양제 추천 서비스
+            </h1>
+            <p className="mx-3 my-4 md:m-6 leading-loose text-base md:text-lg">
+              검색창에 입력하신 문장을 바탕으로
+              <br /> AI가 사용자의 현재 상태를 분석해 <br />
+              건강상태 개선에 도움이 되는 영양제를 추천드리며
+              <br />
+              더 알아보기를 통해 구매까지 가능합니다
+              <br />
+            </p>
+          </div>
+        </div>
+        <div className="py-20 w-fit p-2 flex flex-wrap flex-row-reverse items-center justify-center">
+          <div className="p-2 w-full md:w-5/12 bg-red-400 ">
+            {/* // ! 여기 주소 바꿔줄 것 */}
+            <img src="https://blog.kakaocdn.net/dn/baJ07o/btrGfn1zxpA/qDYoteZOs4bAQ7hXDoo6y0/img.gif" alt="search service" className="rounded-lg shadow-2xl w-[650px]" />
+          </div>
+          <div className="p-2 m-2 break-words text-center">
+            <h1 className="text-4xl md:text-5xl font-bold leading-relaxed md:leading-normal text-white mx-3 md:mx-6">
+              스케쥴러를 통한 영양제 알림
+              <br />
+              생체리듬 체크리스트
+            </h1>
+            <p className="mx-3 my-4 md:m-6 leading-loose text-base md:text-lg">
+              북마크한 영양제를 스케쥴러에 입력하면
+              <br /> 매일 푸시 알림을 통해 아침·점심·저녁별로 리마인드가 가능합니다 <br />
+              생체리듬 체크 리스트를 통해 건강한 생활습관도 만들어가보세요!
+            </p>
+          </div>
+        </div>
+        <div className="py-20 w-fit p-2 flex flex-wrap items-center justify-center">
+          <div className="p-2 w-full md:w-5/12">
+            <img src="https://blog.kakaocdn.net/dn/baJ07o/btrGfn1zxpA/qDYoteZOs4bAQ7hXDoo6y0/img.gif" alt="search service" className="rounded-lg shadow-2xl w-[650px]" />
+          </div>
+          <div className="p-2 m-2 break-words text-center">
+            <h1 className="text-4xl md:text-5xl font-bold leading-relaxed md:leading-normal text-white mx-3 md:mx-6">
+              영양제 북마크 기능
+              <br />
+              맞춤 데이터 분석 결과 제공
+            </h1>
+            <p className="mx-3 my-4 md:m-6 leading-loose text-base md:text-lg">
+              검색 결과로 나온 영양제 중 북마크한 영양제를 볼 수있고
+              <br />
+              회원 가입시 입력한 사용자 정보를 바탕으로
+              <br />내 또래, 내 성별이 많이 먹는 영양제를 알 수 있습니다
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Pmrguide;
