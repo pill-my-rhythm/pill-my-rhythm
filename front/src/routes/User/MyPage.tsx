06ba53de7ce58ef287494b5d607562072d98ece6
YounJoo Seog <reina.seog@gmail.com>
2022-07-01 15:49:39 +0900

    [FE] feat: 생체리듬 잔디 추가

--- front/src/routes/User/MyPage.tsx
+++ front/src/routes/User/MyPage.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import UserBookMarkList from "./UserBookMarkPage";
 import UserMyPage from "./UserMyPage";
 import UserRecommendPage from "./UserRecommendPage";
+import MyYearlyChecklist from "./MyYearlyChecklist";
+import { CurrentuserState } from "../../atoms";
+import { useRecoilValue } from "recoil";
 
 const MyPage = () => {
+  const Recoiluser = useRecoilValue(CurrentuserState);
+  // const refresh = () => {
+  //   if (window.location.href.indexOf("#reload") == -1) window.location.href += "#reload";
+  // };
+
+  // useEffect(() => {
+  //   refresh();
+  // }, []);
+
   return (
     <>
-      <UserMyPage />
+      <UserMyPage Recoiluser={Recoiluser} />
+      <MyYearlyChecklist />
       <UserBookMarkList />
-      <UserRecommendPage />
+      <UserRecommendPage Recoiluser={Recoiluser} />
     </>
   );
 };
