97038898624a8a55dff0f853eed9ca2bd778429e
YounJoo Seog <reina.seog@gmail.com>
2022-07-02 21:33:08 +0900

    [FE] refact: 문구 수정

--- front/src/routes/Search/Result/Promotion.tsx
+++ front/src/routes/Search/Result/Promotion.tsx
@@ -23,13 +23,13 @@ const Promotion = () => {
   return (
     <div className="bg-base-200 w-screen">
       <div className="p-2 break-words text-center">
-        <h1 className="text-3xl md:text-5xl font-bold text-white mx-6 leading-relaxed pt-20 text-teal-500">당신을 위한 영양제, 보기만 하고 넘어가실 건가요?</h1>
+        <h1 className="text-3xl md:text-5xl font-bold text-white mx-6 leading-relaxed pt-20 text-teal-500">당신을 위한 영양제, 보기만 하고 가실건가요?</h1>
         <p className="m-6 leading-6 p-4">
-          내가 챙겨먹는 영양제 알림 등록부터
+          내가 챙겨먹는 영양제 알림 서비스부터
           <br />
-          생체 리듬 회복을 위한 매일의 체크 리스트까지!
+          생체 리듬 회복을 위한 체크 리스트까지!
           <br />
-          Pill my rhythm의 스케쥴러 서비스를 이용해보세요!
+          Pill my rhythm의 스케줄러를 이용해보세요!
         </p>
 
         <button className="btn btn-primary" onClick={ControlScheduler}>
