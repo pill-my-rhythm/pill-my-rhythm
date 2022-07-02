e801d3d1b6688cc633d362eb33c90e6c45f95add
violetwhenisawu <anashape710@gmail.com>
2022-07-02 15:11:42 +0900

    [FE] style : 구독 모달 UI 수정, 영양제 저장 dnd 오류 수정

--- front/src/routes/Schedule/Subscribe.tsx
+++ front/src/routes/Schedule/Subscribe.tsx
@@ -26,7 +26,6 @@ function Subscribe() {
       applicationServerKey: process.env.REACT_APP_WEB_PUSH_PUBLIC_KEY,
     });
 
-    console.log(JSON.stringify(subscription));
     setSubToken(JSON.stringify(subscription));
 
     // 사용자 기기 정보 DB에 추가
@@ -48,7 +47,6 @@ function Subscribe() {
       alert("구독 정보가 없는 기기입니다.");
       return;
     }
-
     // 사용자 기기 정보 DB에서 삭제
     await post("subscribe/delete", { device_token: subscription });
 
@@ -64,50 +62,40 @@ function Subscribe() {
           <p className="text-slate-500 font-medium text-sm">영양제 일정 알림을 받아보세요!</p>
         </div>
 
-        {!subToken ? (
-          <button
-            onClick={() => subscribe()}
-            className="px-4 py-1 text-sm text-teal-600 font-semibold rounded-full border border-teal-200 hover:text-white hover:bg-teal-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-teal-600 focus:ring-offset-2"
-          >
-            Subscribe
-          </button>
-        ) : unSubToken ? (
-          <button
-            onClick={() => subscribe()}
-            className="px-4 py-1 text-sm text-teal-600 font-semibold rounded-full border border-teal-200 hover:text-white hover:bg-teal-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-teal-600 focus:ring-offset-2"
-          >
-            Subscribe
-          </button>
-        ) : (
-          <button
-            onClick={() => unsubscribe()}
-            className="px-4 py-1 text-sm text-teal-600 font-semibold rounded-full border border-teal-200 hover:text-white hover:bg-teal-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-teal-600 focus:ring-offset-2"
-          >
-            unsubscribe
-          </button>
-        )}
-
-        <label
-          htmlFor="QRcode"
-          className="modal-button cursor-pointer px-4 py-1 ml-2 text-sm text-teal-600 font-semibold rounded-full border border-teal-200 hover:text-white hover:bg-teal-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-teal-600 focus:ring-offset-2"
-        >
-          QR
-        </label>
+        <button className="px-0 py-1 text-sm text-teal-600 font-semibold rounded-full border border-teal-200 hover:text-white hover:bg-teal-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-teal-600 focus:ring-offset-2">
+          <label htmlFor="QRcode" className="modal-button cursor-pointer px-4 py-1">
+            구독하러 가기
+          </label>
+        </button>
 
         <input type="checkbox" id="QRcode" className="modal-toggle" />
         <label htmlFor="QRcode" className="modal cursor-pointer">
           <label className="modal-box relative" htmlFor="">
-            <div className="card">
-              <figure>
-                <img src={QRcode} alt="QRcode" width="170" height="170" className="rounded-xl" />
-                <div className="flex-col px-5">
-                  <h2 className="card-title mb-1">모바일 알림 서비스</h2>
-                  <p className="leading-5">
-                    영양제 일정 알림 서비스는 현재 ios에서 지원되지 않아 Android 또는 Web에서만 가능합니다. <br />
-                    Android 접속시 google 애플리케이션의 google lens를 이용하는 것을 추천드립니다.
-                  </p>
-                </div>
-              </figure>
+            <h2 className="card-title mb-1 text-teal-500">영양제 일정 알림 구독 서비스</h2>
+            <div className="divider">Web</div>
+
+            <div className="items-center justify-center flex flex-row space-x-2 p-2">
+              <button
+                onClick={() => subscribe()}
+                className="px-4 py-1 text-sm text-teal-600 font-semibold rounded-full border border-teal-200 hover:text-white hover:bg-teal-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-teal-600 focus:ring-offset-2"
+              >
+                구독하기
+              </button>
+              <button
+                onClick={() => unsubscribe()}
+                className="px-4 py-1 text-sm text-teal-600 font-semibold rounded-full border border-teal-200 hover:text-white hover:bg-teal-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-teal-600 focus:ring-offset-2"
+              >
+                구독 취소
+              </button>
+            </div>
+
+            <div className="divider">Mobile</div>
+            <div className="items-center justify-center flex flex-col">
+              <img src={QRcode} alt="QRcode" width="170" height="170" className="rounded-xl" />
+              <h3 className="text-slate-900 mt-5 text-base font-medium tracking-tight">모바일 알림 구독 QR</h3>
+              <p className="text-slate-500  mt-2 text-sm text-left">
+                영양제 일정 알림 서비스는 현재 ios에서 지원되지 않아 Android 또는 Web에서만 가능합니다. Android 접속시 google 애플리케이션의 google lens를 이용하시는 것을 추천드립니다.
+              </p>
             </div>
           </label>
         </label>
