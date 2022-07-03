f4fa87c6f24df34edbb739456eee84b621dd5277
SongheeNoh <s0n9h2@gmail.com>
2022-07-03 09:42:49 +0900

    [FE] modify: prettier로 인한 변경

--- front/public/service-worker.js
+++ front/public/service-worker.js
@@ -3,33 +3,33 @@
 
 // 서비스 워커에서 발생하는(back에서 보낸) 푸시 이벤트를 수신
 // self는 서비스 워커 자체를 참조
-self.addEventListener('push', event => {
+self.addEventListener("push", (event) => {
   const data = event.data.json();
   const messageType = data.messageType;
 
   const infoMessageOptions = {
-    icon: './icon-192x192.png',
-    badge: './badge-72x72.png', // android에서만 보임
+    icon: "./icon-192x192.png",
+    badge: "./badge-72x72.png", // android에서만 보임
     vibrate: [200, 100, 200, 100, 200, 100, 200], // android에서만 동작
     body: data.body,
   };
 
   const supplementMessageOptions = {
-    icon: './icon-192x192.png',
-    badge: './badge-72x72.png', // android에서만 보임
+    icon: "./icon-192x192.png",
+    badge: "./badge-72x72.png", // android에서만 보임
     vibrate: [200, 100, 200, 100, 200, 100, 200], // android에서만 동작
     // sound: "./"
 
     body: data.body,
-    tag: 'supplement',
+    tag: "supplement",
     actions: [
       {
-        action: 'homepage-action',
-        title: 'Pill my rhythm',
+        action: "homepage-action",
+        title: "Pill my rhythm",
         // icon: "/images/demos/action-1-128x128.png",
       },
       {
-        action: 'checklist-action',
+        action: "checklist-action",
         title: "Today's Checklist",
         // icon: "/images/demos/action-4-128x128.png",
       },
@@ -44,21 +44,25 @@ self.addEventListener('push', event => {
   };
 
   switch (messageType) {
-    case 'info': {
+    case "info": {
       // 브라우저는 전달된 Promise가 확인될 때까지 서비스 워커를 활성화 및 실행 상태로 유지
-      event.waitUntil(self.registration.showNotification(data.title, infoMessageOptions));
+      event.waitUntil(
+        self.registration.showNotification(data.title, infoMessageOptions)
+      );
       break;
     }
-    case 'supplement': {
-      event.waitUntil(self.registration.showNotification(data.title, supplementMessageOptions));
+    case "supplement": {
+      event.waitUntil(
+        self.registration.showNotification(data.title, supplementMessageOptions)
+      );
     }
     // no default
   }
 });
 
 self.addEventListener(
-  'notificationclick',
-  event => {
+  "notificationclick",
+  (event) => {
     // 알림창 닫음
     event.notification.close();
     // close all notifications
@@ -70,14 +74,20 @@ self.addEventListener(
 
     // User selected the Archive action.
     switch (event.action) {
-      case 'homepage-action':
+      case "homepage-action":
         // URL을 로드하는 새 창이나 탭이 열림
-        event.waitUntil(self.clients.openWindow('https://kdt-ai4-team17.elicecoding.com'));
+        event.waitUntil(
+          self.clients.openWindow("https://kdt-ai4-team17.elicecoding.com")
+        );
         break;
-      case 'checklist-action': // 오늘 날짜의 체크리스트
+      case "checklist-action": // 오늘 날짜의 체크리스트
         const { encryptedToken } = event.notification.data;
         // public 폴더 안에서는 .env 변수 접근 안 됨
-        event.waitUntil(self.clients.openWindow(`https://kdt-ai4-team17.elicecoding.com/m/checklist?token=${encryptedToken}`));
+        event.waitUntil(
+          self.clients.openWindow(
+            `https://kdt-ai4-team17.elicecoding.com/m/checklist?token=${encryptedToken}`
+          )
+        );
         break;
       // no default
     }
