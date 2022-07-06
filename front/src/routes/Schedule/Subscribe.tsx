3f4fc35c6b237dbceefa4e1971e16085570ba6ec
SongheeNoh <s0n9h2@gmail.com>
2022-07-07 03:45:35 +0900

    [FE] add: 구독에 id 추가

--- front/src/routes/Schedule/Subscribe.tsx
+++ front/src/routes/Schedule/Subscribe.tsx
@@ -55,7 +55,7 @@ function Subscribe() {
     setUnSubToken(JSON.stringify(subscription));
   };
   return (
-    <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+    <div id="subscribeService" className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
       <div className="text-center space-y-2 sm:text-left">
         <div className="space-y-0.5">
           <p className="text-md text-black font-semibold mb-1">구독 서비스</p>
