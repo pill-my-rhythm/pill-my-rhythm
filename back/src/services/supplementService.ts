c5fb778175b6a4a60bfff0753c3af515e4a30333
parkjungyoon <tbr06057@naver.com>
2022-07-13 00:19:00 +0900

    [BE] add: 영양제 이름 검색 추가 (service)

--- back/src/services/supplementService.ts
+++ back/src/services/supplementService.ts
@@ -2,8 +2,8 @@ import { Supplement } from "../db/Supplement";
 import { HttpException } from "../utils/error-util";
 
 const SupplementService = {
-  getAllSupplements: async (pageNum: number) => {
-    const supplements = await Supplement.getAllSupplements(pageNum);
+  getAllSupplements: async (pageNum: number, search: string) => {
+    const supplements = await Supplement.getAllSupplements(pageNum, search);
     if (!supplements) {
       throw new HttpException(401, "영양제 데이터가 없습니다.");
     }
