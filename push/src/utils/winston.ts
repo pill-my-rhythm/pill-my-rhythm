7170a527b507981d439878b9ad6b4cbcb96527cf
SongheeNoh <s0n9h2@gmail.com>
2022-07-13 02:17:59 +0900

    [push] add: winston 설정 push.log와 error.log로 나누기

--- push/src/utils/winston.ts
+++ push/src/utils/winston.ts
@@ -1,40 +1,29 @@
-// import fs from "fs";
-// import moment from "moment";
-// import "moment-timezone";
-// import winston from "winston";
-// import "winston-daily-rotate-file";
+import fs from "fs";
+import winston from "winston";
+import "winston-daily-rotate-file";
 
-// const logDir = __dirname + "/../logs";
+const logDir = __dirname + "/../logs";
 
-// if (!fs.existsSync(logDir)) {
-//   fs.mkdirSync(logDir);
-// }
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
 
-// const infoTransport = new winston.transports.DailyRotateFile({
-//   filename: "info.log",
-//   dirname: logDir,
-//   level: "info",
-//   maxFiles: "30d", // 30일치 저장
-// });
+const pushTransport = new winston.transports.DailyRotateFile({
+  filename: "push.log",
+  dirname: logDir,
+  level: "info",
+  maxFiles: "30d", // 30일치 저장
+});
 
-// const errorTransport = new winston.transports.DailyRotateFile({
-//   filename: "error.log",
-//   dirname: logDir,
-//   level: "error",
-//   maxFiles: "30d", // 30일치 저장
-// });
+const errorTransport = new winston.transports.DailyRotateFile({
+  filename: "error.log",
+  dirname: logDir,
+  level: "error",
+  maxFiles: "30d", // 30일치 저장
+});
 
-// moment.tz.setDefault("Asia/Seoul"); // 로그 시간대 한국 기준으로 변경
-// const timeStamp = () => moment().format("YYYY-MM-DD HH:mm:ss");
+const logger = winston.createLogger({
+  transports: [pushTransport, errorTransport],
+});
 
-// const logger = winston.createLogger({
-//   transports: [infoTransport, errorTransport],
-// });
-
-// const stream = {
-//   write: (message: string) => {
-//     logger.info(`${timeStamp()} ${message}`);
-//   },
-// };
-
-// export { logger, stream };
+export { logger };
