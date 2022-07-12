458b920a85a5a6231a5a58adfa08bef958b9e5d2
parkjungyoon <tbr06057@naver.com>
2022-07-13 00:19:22 +0900

    [BE] add: 영양제 이름 검색 쿼리 추가

--- back/src/db/Supplement.ts
+++ back/src/db/Supplement.ts
@@ -1,29 +1,59 @@
+import { Op } from "./models";
 import { Supplements } from "./models/supplement";
 
 const Supplement = {
-  getAllSupplements: async (pageNum: number) => {
+  getAllSupplements: async (pageNum: number, search: string) => {
     let offset = 0;
     if (pageNum > 1) {
       offset = 15 * (pageNum - 1);
     }
-    const supplements = await Supplements.findAll({
-      // pagination
-      offset: offset,
-      limit: 15,
-      attributes: [
-        "pk_supplement_id",
-        "update_date",
-        "shape",
-        "name",
-        "caution",
-        "company",
-        "function",
-        "how_to_eat",
-        "raw",
-        "img_link",
-        "link",
-      ],
-    });
+
+    let supplements;
+    if (search == undefined) {
+      supplements = await Supplements.findAll({
+        // pagination
+        offset: offset,
+        limit: 15,
+        attributes: [
+          "pk_supplement_id",
+          "update_date",
+          "shape",
+          "name",
+          "caution",
+          "company",
+          "function",
+          "how_to_eat",
+          "raw",
+          "img_link",
+          "link",
+        ],
+      });
+    } else {
+      supplements = await Supplements.findAll({
+        where: {
+          name: {
+            [Op.like]: "%" + search + "%",
+          },
+        },
+        // pagination
+        offset: offset,
+        limit: 15,
+        attributes: [
+          "pk_supplement_id",
+          "update_date",
+          "shape",
+          "name",
+          "caution",
+          "company",
+          "function",
+          "how_to_eat",
+          "raw",
+          "img_link",
+          "link",
+        ],
+      });
+    }
+
     return supplements;
   },
 };
