d3ec60f4d2ba9d8d52c670bd866429b49045d977
SongheeNoh <s0n9h2@gmail.com>
2022-07-13 00:36:03 +0900

    [push] modify: morgan은 의미없어서 삭제

--- push/src/app.ts
+++ push/src/app.ts
@@ -1,7 +1,5 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-// import morgan from "morgan";
-// import { stream } from "./utils/winston";
 
 import { SubscribeRouter } from "./routes/subscribeRouter";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
@@ -11,8 +9,6 @@ const app: express.Application = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// morgan (request, response formatting)
-// app.use(morgan("combined", { stream }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hello typescript express!");
