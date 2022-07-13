1743c3864c96884730abfee000baf87e42979c75
parkjungyoon <tbr06057@naver.com>
2022-07-14 02:25:38 +0900

    [BE] add: 쿼리추가: raw(원재료)로 검색

--- back/src/db/Supplement.ts
+++ back/src/db/Supplement.ts
@@ -2,14 +2,14 @@ import { Op } from "./models";
 import { Supplements } from "./models/supplement";
 
 const Supplement = {
-  getAllSupplements: async (pageNum: number, search: string) => {
+  getAllSupplements: async (pageNum: number, search_name: string, search_raw: string) => {
     let offset = 0;
     if (pageNum > 1) {
       offset = 15 * (pageNum - 1);
     }
 
     let supplements;
-    if (search == undefined) {
+    if (search_name == undefined && search_raw == undefined) {
       supplements = await Supplements.findAll({
         // pagination
         offset: offset,
@@ -28,11 +28,35 @@ const Supplement = {
           "link",
         ],
       });
-    } else {
+    } else if (search_name !== undefined) {
       supplements = await Supplements.findAll({
         where: {
           name: {
-            [Op.like]: "%" + search + "%",
+            [Op.like]: "%" + search_name + "%",
+          },
+        },
+        // pagination
+        offset: offset,
+        limit: 15,
+        attributes: [
+          "pk_supplement_id",
+          "update_date",
+          "shape",
+          "name",
+          "caution",
+          "company",
+          "function",
+          "how_to_eat",
+          "raw",
+          "img_link",
+          "link",
+        ],
+      });
+    } else if (search_raw !== undefined) {
+      supplements = await Supplements.findAll({
+        where: {
+          name: {
+            [Op.like]: "%" + search_raw + "%",
           },
         },
         // pagination
