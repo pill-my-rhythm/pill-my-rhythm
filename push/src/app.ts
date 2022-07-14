ec5d55c095f31ca8a3d52951973199789b27173e
parkjungyoon <tbr06057@naver.com>
2022-07-15 08:53:53 +0900

    [push] app.ts 경로 수정

--- push/src/app.ts
+++ push/src/app.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
+import express, { Request, Response } from 'express';
+import cors from 'cors';
 
-import { SubscribeRouter } from "./routes/subscribeRouter";
-import { errorMiddleware } from "./middlewares/errorMiddleware";
+import { SubscribeRouter } from './routes/subscribeRouter';
+import { errorMiddleware } from './middlewares/errorMiddleware';
 
 const app: express.Application = express();
 
@@ -10,11 +10,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("hello typescript express!");
+app.get('/push', (req: Request, res: Response) => {
+  res.send('hello typescript express!');
 });
 
-app.use("/subscribe", SubscribeRouter);
+app.use('/push/subscribe', SubscribeRouter);
 app.use(errorMiddleware);
 
 export default app;
