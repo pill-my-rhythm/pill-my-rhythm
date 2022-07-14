f6ba58ea5508b659c870233947052b9af04e190f
violetwhenisawu <anashape710@gmail.com>
2022-07-15 00:10:05 +0900

    [FE] modify : 검색 페이지 Search 버튼 연결

--- front/src/routes/SupSearch/SupSearch.tsx
+++ front/src/routes/SupSearch/SupSearch.tsx
@@ -90,13 +90,15 @@ function SupSearch() {
                       d="M10.6002 12.0498C9.49758 12.8568 8.13777 13.3333 6.66667 13.3333C2.98477 13.3333 0 10.3486 0 6.66667C0 2.98477 2.98477 0 6.66667 0C10.3486 0 13.3333 2.98477 13.3333 6.66667C13.3333 8.15637 12.8447 9.53194 12.019 10.6419C12.0265 10.6489 12.0338 10.656 12.0411 10.6633L15.2935 13.9157C15.6841 14.3063 15.6841 14.9394 15.2935 15.33C14.903 15.7205 14.2699 15.7205 13.8793 15.33L10.6269 12.0775C10.6178 12.0684 10.6089 12.0592 10.6002 12.0498ZM11.3333 6.66667C11.3333 9.244 9.244 11.3333 6.66667 11.3333C4.08934 11.3333 2 9.244 2 6.66667C2 4.08934 4.08934 2 6.66667 2C9.244 2 11.3333 4.08934 11.3333 6.66667Z"
                     />
                   </svg>
-                  <Form onSubmit={handleSubmit}>
+                  <Form id="SearchForm" onSubmit={handleSubmit}>
                     <SearchInp placeholder="영양제 검색하기" onChange={handleChange} />
                   </Form>
                 </LsWrapper>
                 <VerticalDvd />
                 <BtnWrapper>
-                  <button className="btn btn-ghost">Search</button>
+                  <button type="submit" form="SearchForm" className="btn btn-ghost">
+                    Search
+                  </button>
                 </BtnWrapper>
               </Input>
             </InputWrapper>
