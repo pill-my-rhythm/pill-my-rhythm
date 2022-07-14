ae4d317f1fd5c2047101dd987c705b4946e88060
SongheeNoh <s0n9h2@gmail.com>
2022-07-15 06:42:34 +0900

    [push] fix: 테스트 위한 콘솔로그  추가, 시간 다시 원래대로 변경

--- push/src/utils/webPush.ts
+++ push/src/utils/webPush.ts
@@ -1,12 +1,12 @@
-import axios from 'axios';
-import webpush from 'web-push';
-import schedule from 'node-schedule';
-import moment from 'moment';
-import 'moment-timezone';
-import { logger } from '../utils/winston';
-import { emailUtil } from '../utils/emailUtil';
+import axios from "axios";
+import webpush from "web-push";
+import schedule from "node-schedule";
+import moment from "moment";
+import "moment-timezone";
+import { logger } from "../utils/winston";
+import { emailUtil } from "../utils/emailUtil";
 
-moment.tz.setDefault('Asia/Seoul'); // 로그 시간대 한국 기준으로 변경
+moment.tz.setDefault("Asia/Seoul"); // 로그 시간대 한국 기준으로 변경
 
 // VAPID keys should only be generated only once.
 const vapidKeys = {
@@ -15,21 +15,25 @@ const vapidKeys = {
 };
 
 export default (): void => {
-  webpush.setVapidDetails('mailto:s0n9h2@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
+  webpush.setVapidDetails(
+    "mailto:s0n9h2@gmail.com",
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+  );
 };
 
 // 배포하면 주소 변경해야 함
-// const serverUrl = "http://localhost:" + process.env.PORT + "/";
 // const serverUrl = "http://localhost:5004/";
-const serverUrl = 'https://kdt-ai4-team17.elicecoding.com/push/';
+const serverUrl = "https://kdt-ai4-team17.elicecoding.com/push/";
 
 const push = async (time: Date) => {
   try {
     await axios
-      .get(serverUrl + 'subscribe/push-supplements', {
+      .get(serverUrl + "subscribe/push-supplements", {
         params: { time: time },
       })
-      .then(res => {
+      .then((res) => {
+        console.log(res);
         const pushResultObj = {
           status: res.status,
           statusText: res.statusText,
@@ -54,7 +58,7 @@ const push = async (time: Date) => {
     console.log(`${time}에 설정된 푸시 알림 전송에 실패했습니다.`);
 
     const data = {
-      developers: ['s0n9h2@gmail.com', 'tbr06057@naver.com'],
+      developers: ["s0n9h2@gmail.com", "tbr06057@naver.com"],
       time: time,
       errorContent: pushErrorObj,
     };
@@ -66,9 +70,9 @@ const push = async (time: Date) => {
 
 const rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = new schedule.Range(0, 6); // 매일
-rule.hour = [0, new schedule.Range(1, 23)]; // 오전 6시부터 자정까지
+rule.hour = [0, new schedule.Range(6, 23)]; // 오전 6시부터 자정까지
 rule.minute = [0, 30]; // 정각과 30분에
-rule.tz = 'Asia/Seoul'; // 한국 시간 기준
+rule.tz = "Asia/Seoul"; // 한국 시간 기준
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const job = schedule.scheduleJob(rule, () => {
